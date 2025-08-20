f8bd2df5875b8c54f2f38974eddd509ea5122909
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-08-20 17:42:40 +0530

    Time: 0 ms (100%), Space: 42.3 MB (58.72%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,109 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        
+        // // level order traversal:
+        // List<List<Integer>> lst = new ArrayList<>();
+        // if (root == null) return lst;
+
+        // Queue<TreeNode> q = new LinkedList<>();
+
+        // q.add(root);
+        // int level = 0;
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+
+        //     List<Integer> temp = new ArrayList<>();
+
+        //     for (int i = 0; i < size; i++){
+        //         TreeNode front = q.poll();
+
+        //         temp.add(front.val);
+
+        //         if (front.left != null){
+        //             q.add(front.left);
+        //         }
+
+        //         if (front.right != null){
+        //             q.add(front.right);
+        //         }
+        //     }
+
+        //     if (level % 2 == 1){
+        //         Collections.reverse(temp);
+        //     }
+
+        //     lst.add(temp);
+
+        //     level++;
+
+        // }
+
+        // return lst;
+
+
+
+
+
+
+
+
+
+
+
+
+       
+        List<List<Integer>> lst = new ArrayList<>();
+        if (root == null) return lst;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        int level = 0;
+
+    
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+
+            }
+
+            if (level % 2 != 0){
+                Collections.reverse(temp);
+            }
+
+            lst.add(temp);
+
+            level ++;
+        }
+
+        return lst;
+    }
+}
