9f582ac272f7f9cfd479740148511f26169c8a87
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-08-22 19:45:04 +0530

    Time: 1530 ms (7.38%), Space: 67.8 MB (70.49%) - LeetHub

---
+++ 1845-largest-submatrix-with-rearrangements/1845-largest-submatrix-with-rearrangements.java
@@ -0,0 +1,63 @@
+class Solution {
+    
+    private void reverse(int [] heights){
+        int n = heights.length;
+        int start = 0;
+        int end = n-1;
+
+        while(start < end){
+            int temp = heights[start];
+            heights[start] = heights[end];
+            heights[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    private int LargestArea(int [] heights){
+        int maxi = 0;
+        Arrays.sort(heights);
+        reverse(heights);
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++){
+            maxi = Math.max(maxi,  Math.max(heights[i], heights[i]*(i+1)));
+        }
+
+        return maxi;
+    }
+    public int largestSubmatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int [][] temp = new int [n][m];
+        Stack<Integer> st = new Stack<>();
+
+        for (int col = 0; col < m; col++){
+            for (int row = 0; row < n; row++){
+                if (row == 0){
+                    temp[row][col] = matrix[row][col];
+                    continue;
+                }
+                if (matrix[row][col] == 1){
+                    temp[row][col] = matrix[row][col] + temp[row-1][col];
+                } 
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                System.out.print(temp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++){
+            maxArea = Math.max(maxArea, LargestArea(temp[i]));
+        }
+
+        return maxArea;
+    }
+}
