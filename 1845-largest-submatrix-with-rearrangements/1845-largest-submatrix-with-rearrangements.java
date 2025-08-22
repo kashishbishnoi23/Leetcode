6054d559dfa73f041dfdd1eb2de2311743260ee2
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-08-22 19:45:16 +0530

    Time: 15 ms (44.26%), Space: 67.6 MB (71.31%) - LeetHub

--- 1845-largest-submatrix-with-rearrangements/1845-largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/1845-largest-submatrix-with-rearrangements.java
@@ -44,13 +44,6 @@ class Solution {
             }
         }
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                System.out.print(temp[i][j] + " ");
-            }
-            System.out.println();
-        }
-
 
         int maxArea = 0;
 
