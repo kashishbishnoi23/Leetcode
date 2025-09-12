d2ae894cf2cd3eece6c64968a08dba9ab17f2d53
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-09-12 18:58:37 +0530

    Time: 11 ms (6.31%), Space: 41.6 MB (21.86%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static long sum = 0;
+    private void recursive(TreeNode root, StringBuilder sb){
+        if (root == null) return;
+        if (root.left == null && root.right == null){
+            sb.append(root.val);
+            
+            long num = sb.isEmpty() ? 0 : Long.valueOf(sb.toString());
+            sum += num;
+            System.out.println("num = " + num);
+            System.out.println("sum = " + sum);
+            if (!sb.isEmpty())sb.deleteCharAt(sb.length()-1);
+            return;
+        }
+
+        // go to left,left left until null, if null take the number, sum it up , erase that number and go to right 
+        
+        sb.append(root.val);
+
+        recursive(root.left, sb);
+        
+
+        recursive(root.right, sb);
+        if (!sb.isEmpty()) sb.deleteCharAt(sb.length()-1);
+
+    }
+
+    public int sumNumbers(TreeNode root) {
+        sum = 0;
+        StringBuilder sb = new StringBuilder();
+        recursive(root, sb);
+
+        return (int)sum;
+    }
+}
