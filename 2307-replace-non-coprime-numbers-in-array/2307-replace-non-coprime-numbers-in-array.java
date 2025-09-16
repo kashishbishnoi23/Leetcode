ea498e7c25f6c90dc7dfc594fab226a82038410d
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-09-16 18:21:29 +0530

    Time: 76 ms (24.74%), Space: 65.3 MB (22.68%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
@@ -1,45 +1,52 @@
 class Solution {
 
-      // Compute GCD using Euclid's algorithm
-    private long gcd(long a, long b) {
-        while (b != 0) {
-            long temp = b;
+    private int gcd(int a, int b){
+        if (a < b){
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        while(b != 0){
+            int temp = b;
             b = a % b;
             a = temp;
         }
+
         return a;
     }
 
-    // Compute LCM using GCD
-    private long lcm(long a, long b) {
-        return a / gcd(a, b) * b; // safe long computation
+    private int lcm(int x, int y){
+        long lcm =  ((long) x*y)/gcd(x,y);
+        return (int)lcm;
     }
-
-    public List<Integer> replaceNonCoprimes(int[] nums) {
-        Stack<Long> stack = new Stack<>();
-        
-        for (int num : nums) {
-            long cur = num; // cast int to long
-
-            // Merge repeatedly with stack top until co-prime
-            while (!stack.isEmpty() && gcd(stack.peek(), cur) > 1) {
-                cur = lcm(stack.pop(), cur);
-            }
-
-            stack.push(cur);
-        }
-
-        // Convert stack to List<Integer> safely
-        List<Integer> ans = new ArrayList<>();
-        for (long x : stack) {
-            ans.add((int)x); // safe because final values <= 10^8
-        }
-
-        return ans;
     
 
+    
 
-        // return ans;
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+       Stack<Integer> st = new Stack<>();
+
+       for (int num : nums){
+        int curr = num;
+
+        while(!st.isEmpty() && gcd(curr, st.peek()) > 1){
+           curr = lcm(curr, st.pop());
+        }
+
+        
+        st.push(curr); // if stack is empty or co-prime found
+       }
+
+       List<Integer> ans = new ArrayList<>();
+       while(!st.isEmpty()){
+           ans.add(st.pop());
+       }
+       Collections.reverse(ans);
+
+       return ans;
+
+
 
 
     }
