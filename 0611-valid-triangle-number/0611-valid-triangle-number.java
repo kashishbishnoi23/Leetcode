29e170d378cea30d56b818c9bde4bab4267e445e
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-09-26 11:12:48 +0530

    Time: 23 ms (85.21%), Space: 43.6 MB (27.37%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+
+        for (int i = 2; i < nums.length; i++){
+            // nums[i] is my largest side:
+            int left = 0;
+            int right = i-1;
+
+            while(left < right){
+                if (nums[left] + nums[right] > nums[i]){
+                    count += right-left;
+                    right--;
+                }else {
+                    left++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
