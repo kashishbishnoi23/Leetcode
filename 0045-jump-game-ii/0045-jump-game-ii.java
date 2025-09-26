0f86f692a71efeeb38520e007ac643f39617942e
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-09-26 18:24:05 +0530

    Time: 49 ms (11.03%), Space: 45.7 MB (18.18%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,10 +1,8 @@
 class Solution {
 
     private int recursive(int[] nums, int index, int [] dp){
-         
-   
         if (index >= nums.length-1) return 0;
-             if (dp[index] != Integer.MAX_VALUE) return dp[index];
+        if (dp[index] != Integer.MAX_VALUE) return dp[index];
         int min = Integer.MAX_VALUE;
         if (nums[index] == 0) return Integer.MAX_VALUE;
         for (int i = 1; i <= nums[index]; i++){
@@ -17,10 +15,26 @@ class Solution {
         return dp[index] =  min;
     }
     public int jump(int[] nums) {
-
         int n = nums.length;
         int [] dp = new int [n];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        return recursive(nums, 0, dp);
+        // return recursive(nums, 0, dp);
+
+        dp[n-1] = 0;
+
+        for (int i = n-2; i >= 0; i--){
+            for (int j = 1; j <= nums[i]; j++){
+                // take j steps:
+                if (i+j >= n) continue;
+                int next = dp[i+j];
+                if (next != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], 1+next);
+                }
+            }
+        }
+
+        return dp[0];
+
+
     }
 }
