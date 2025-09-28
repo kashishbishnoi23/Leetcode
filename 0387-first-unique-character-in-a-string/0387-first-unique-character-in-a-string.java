aa4743c1f4ab580fdae20f8a5ef6f12d1fd961b2
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-09-28 19:57:55 +0530

    Time: 650 ms (5.33%), Space: 46.1 MB (7.05%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -2,15 +2,35 @@ class Solution {
     public int firstUniqChar(String s) {
         int n = s.length();
 
+        // for (int i = 0; i < n; i++){
+        //     char ch =  s.charAt(i);
+        //     int count = 0;
+        //     for (int j = 0; j < n;j ++){
+        //           if (s.charAt(j) == ch) count++;
+        //           if (count > 1) break;
+        //     }
+        //     if (count == 1) return i;
+        // }
+
+        Map<Character, ArrayList<Integer>> hashing = new LinkedHashMap<>();
         for (int i = 0; i < n; i++){
-            char ch =  s.charAt(i);
-            int count = 0;
-            for (int j = 0; j < n;j ++){
-                  if (s.charAt(j) == ch) count++;
-                  if (count > 1) break;
+            char key = s.charAt(i);
+            if (!hashing.containsKey(s.charAt(i))){
+                hashing.put(key, new ArrayList<>());               
+                hashing.get(key).add(i);
+                hashing.get(key).add(1);
+                continue;
             }
-            if (count == 1) return i;
+            int count = hashing.get(key).get(1);
+            hashing.get(key).add(1, count+1);
         }
+
+        for (char key : hashing.keySet()){
+            System.out.println(key);
+            if (hashing.get(key).get(1) == 1) return hashing.get(key).get(0); 
+        }
+
+        
 
         return -1;
 
