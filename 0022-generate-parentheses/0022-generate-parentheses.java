7c9126d06a45d72d00aded899968c1903c8752af
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-09-29 14:42:46 +0530

    Time: 19 ms (5.05%), Space: 43.5 MB (34.09%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    
+     private void helper(int n, ArrayList<String> ans, StringBuilder sb, int open, int close){
+     
+        if (open > n || close > n) return;
+            if (open == n && close == n){
+                   System.out.println("open = " + open + " close = " + close);
+        System.out.println("sb = " + sb);
+                ans.add(sb.toString());
+                return;
+            }
+
+            // add open bracket:
+            sb.append('(');
+            helper(n, ans, sb, open+1, close);
+
+
+            // add close bracket:
+            if (open != close){
+            sb.deleteCharAt(sb.length()-1);
+            sb.append(')');
+            helper(n, ans, sb, open, close+1);
+            }
+           if (sb.length() > 1) sb.deleteCharAt(sb.length()-1);
+     }
+
+    public List<String> generateParenthesis(int n) {
+         ArrayList<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(n, ans, sb, 0,0);
+         return ans;
+    }
+}
