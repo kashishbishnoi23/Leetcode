a59583beb4105abca45df76a1da79a6e135b346e
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-09-29 18:04:35 +0530

    Time: 124 ms (5.22%), Space: 45.4 MB (6.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,10 @@
 class Solution {
 
-    private boolean helper(int indx, String s, Stack<Character>st){
-        if (indx == s.length()){
-            if (st.isEmpty()) return true;
-            return false;
-        }
-
-        char bracket = s.charAt(indx);
-
-        if (bracket == '[' || bracket == '(' || bracket == '{'){
-            st.push(bracket);
-        } else {
-            if (st.isEmpty()) return false;
-            if (bracket == ']' && st.peek() == '[' || bracket == '}' && st.peek() == '{' || bracket == ')' && st.peek() == '('){
-                st.pop();
-            } else {
-                return false;
-            }
-        }
-
-        return helper(indx+1, s, st);
-    }
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        return helper(0, s, st);
+    String replaced = s.replace("()", "")
+                       .replace("{}", "")
+                       .replace("[]", "");
+    if (replaced.equals(s)) return s.isEmpty(); // no change
+    return isValid(replaced);
     }
 }
