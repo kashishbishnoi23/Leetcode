9dfd69eb789f5d9b35605e3d4e02851424b62931
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-01 19:45:26 +0530

    Time: 1 ms (99.73%), Space: 45.6 MB (81.99%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,37 +1,34 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        // so basically, I have to find the first position where the bit differs , so one of them has bit 0 and the other has bit 1 , so now divide all the elements into 2 groups , one group has 1 at that position and other has 0, so a and b will go into 2 different groups , now xor of each group will give us numbers cuz the pairs will be cancelled out
-
-        int size = nums.length;
-        int xor = 0;
-        for (int num : nums){
-            xor ^= num;
+         int xor = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++){
+            xor ^= nums[i];
         }
 
-        // find the first set bit from right:
-        // int i = xor & (-xor);  will not work for larger numbers:
-        int i = 0;
-        for (int k = 0; k < 32; k++){
-            if (((1 << k) & xor) != 0){
-                i = k;
+        // create two groups based on the first different bit:
+        int position = -1;
+        // find the first different bit:
+        for (int i = 0; i < 32; i++){
+            if ((xor & (1 << i)) != 0){
+                position = i;
                 break;
             }
         }
-
-        // form 2 groups a and b:
         int a = 0;
         int b = 0;
 
-        for (int num : nums){
-            // check the ith bit of num:
-            if ((num & (1 << i)) != 0){
-                a ^= num;
+        // create two groups:
+        for (int i = 0; i < n; i++){
+            // if the different bit is 0, push to a
+            if ((nums[i] & (1 << position)) == 0){
+                 a ^= nums[i];
             } else {
-                b ^= num;
+                // else push to group b
+                b ^= nums[i];
             }
         }
 
-        return new int[]{a, b};
-
+        return new int[] {a, b};
     }
 }
