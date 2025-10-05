a6bbf9d48bbbc2153b085afce4884c695ae03b5c
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-05 14:25:18 +0530

    Time: 0 ms (100%), Space: 42.6 MB (97.5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,33 +1,33 @@
 class Solution {
 
-    
-     private void helper(int n, ArrayList<String> ans, StringBuilder sb, int open, int close){
-     
-        if (open > n || close > n) return;
-            if (open == n && close == n){
-                ans.add(sb.toString());
-                return;
-            }
+    private void recursive(List<String> ans, StringBuilder sb, int n, int open, int close){
+        if (close == n){
+            ans.add(new String(sb));
+            return;
+        }
 
-            // add open bracket:
+        // add open:
+        if (open < n){
             sb.append('(');
-            helper(n, ans, sb, open+1, close);
-            
-
-
-            // add close bracket:
-            if (open != close){
+            recursive(ans, sb, n, open+1, close);
             sb.deleteCharAt(sb.length()-1);
-            sb.append(')');
-            helper(n, ans, sb, open, close+1);
-            }
-           if (sb.length() > 1) sb.deleteCharAt(sb.length()-1);
-     }
+        }
 
+        // add close
+        if (open > close){
+            sb.append(')');
+            recursive(ans, sb, n, open, close+1);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        
+
+
+    }
     public List<String> generateParenthesis(int n) {
-         ArrayList<String> ans = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        helper(n, ans, sb, 0,0);
-         return ans;
+
+        recursive(ans, sb, n, 0, 0);
+        return ans;
     }
 }
