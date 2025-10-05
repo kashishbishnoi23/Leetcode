dd4e0ee45797806c724ec109db1dc3c04dba7e9b
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-05 17:10:19 +0530

    Time: 1 ms (92.73%), Space: 41.5 MB (84.34%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.java
+++ 0468-validate-ip-address/0468-validate-ip-address.java
@@ -4,23 +4,23 @@ class Solution {
         if (str.length() > 1 && str.startsWith("0")) return false;
 
         // can be converted to integer:
-        boolean isInteger = true;
+        // boolean isInteger = true;
         int number = -1;
 
         try{
             number = Integer.parseInt(str);
         } catch(NumberFormatException e){
-            isInteger = false;
+            // isInteger = false;
+            return false;
         }
-        System.out.println(number);
-        if (isInteger && number >= 0 && number <= 255) return true;
+        // System.out.println(number);
+        if (number >= 0 && number <= 255) return true;
 
         return false;
     }
 
     private boolean isValidIPV6(String str){
-        if (str.isEmpty()) return false;
-        if (str.length() > 4) return false;
+        if (str.isEmpty() || str.length() > 4) return false;
 
         for (int i = 0; i < str.length(); i++){
             char ch = str.charAt(i);
@@ -31,25 +31,23 @@ class Solution {
         } 
         return true;
     }
+
     public String validIPAddress(String queryIP) {
+
         if (queryIP.indexOf('.') != -1){
             // check for IPV4
-            if (queryIP.endsWith(".")) return "Neither";
             String [] parts = queryIP.split("\\.");
-            System.out.println(parts.length);
-            if (parts.length != 4) return "Neither";
+            if (queryIP.endsWith(".") || parts.length != 4) return "Neither";
            
             for (int i = 0; i < parts.length; i++){
                 if (!isValidIPV4(parts[i])) return "Neither";
             }
+
             return "IPv4";
         } else if (queryIP.indexOf(':') != -1){
             // check for IPV6
-
-            if (queryIP.endsWith(":")) return "Neither";
-
             String[] parts = queryIP.split(":");
-            if (parts.length  != 8) return "Neither";
+            if (queryIP.endsWith(":") || parts.length != 8) return "Neither";
 
             for (int j = 0; j < parts.length; j++){
                 if (!isValidIPV6(parts[j])) return "Neither";
