bbcbcb26c9d2286e7c65eb7aad6fcfa2bad04cc5
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-06 10:52:06 +0530

    Time: 4 ms (95.52%), Space: 45.3 MB (6.48%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,115 +1,74 @@
 class Solution {
 
-    private boolean canReach(int mid, boolean [][] visited, int row, int col, int n, int [][] grid ){
+    private boolean canReach(int max, int[][] grid, int row, int col, boolean[][] visited){
+         
+         int n = grid.length;
+         // base case:
+         if (row == n-1 && col == n-1) return true;
 
-        // check validity -> 
-        if (!isValid(row, col, n) || visited[row][col] || grid[row][col] > mid ) return false; // if the current height > mid -> return false
-        
+         // mark as visited:
+         visited[row][col] = true;
+         int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
 
-        if (row == n-1 && col == n-1) return true; // if reached destination => return true
+         // go to its neighbours:
 
-        visited[row][col] = true;
-
-        int [][] directions = {{-1, 0}, {0,-1}, {0,1}, {1,0}};
-
-        // go to all directions:
         for (int [] direction : directions){
             int newrow = row + direction[0];
             int newcol = col + direction[1];
 
-            if (canReach(mid, visited, newrow, newcol, n, grid)){
-                return true;
+            if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < n && visited[newrow][newcol] == false && grid[newrow][newcol] <= max){
+                visited[newrow][newcol] = true;
+                if (canReach(max, grid, newrow, newcol, visited)) return true;
+
             }
+
+
         }
 
         return false;
 
-    }
 
-    private boolean isValid(int row, int col, int n){
-       return row >= 0 && row < n && col >= 0 && col < n;
+
     }
     public int swimInWater(int[][] grid) {
+         
+        // kisi bhi elevation pe ham tak swim kr skte hain if its height is less than or equal to the water level (t)
+
+        // max time jo lag skta hai -> that's max value in the grid => us elevation ko doobne me max time lagega
+        // Binary Search solution -> start = 0, end = max
+
+        // find mid -> see if u can reach (n-1, n-1) in mid time -> if yes -> then minimize the time => by doing end = mid-1
+        // if not possible to reach in mid time => time badhana padega => start = mid + 1
+
+        int max = 0;
+
+        // O(N^2)
         int n = grid.length;
-
-        // binary search:
-        // find the max height of the elevation -> that's the max time that can be used to reach the destination:
-
-        /*
-        int maxHeight = 0;
-
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                maxHeight = Math.max(maxHeight, grid[i][j]);
-                // itna time maximum lagega => cuz this is the max height of all elevations -> inse kam height wale bhi vo swim kr skta hai 
+                max = Math.max(max, grid[i][j]);
             }
         }
 
         int start = 0;
-        int end = maxHeight;
+        int end = max;
 
-        int mid = (start) + (end-start)/2;
-
-        while(start <= end){
-            mid = start + (end-start)/2;
-
-            boolean [][] visited = new boolean [n][n];
-
-            // check if we can reach in mid time:
-            if (canReach(mid, visited, 0,0, n, grid)){ // dfs se check if we can reach in this time
-                // try minimum time:
-                end = mid-1;
+        while(start < end){
+            int mid = start + (end - start )/2;
+            // check if it is possible with mid:
+            boolean[][] visited = new boolean[n][n];
+            if (grid[0][0] > mid){
+                start = mid+1;
+                continue;
+            }
+            if (canReach(mid, grid, 0, 0, visited)){
+                end = mid;
             } else {
-                // take more time:
                 start = mid+1;
             }
 
-            System.out.println("mid = " + mid);
         }
 
         return start;
-        
-        */
-
-        // Solution 2 :  Dijistra algorithm:
-        int time = grid[0][0];
-
-    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
-        pq.add(new int [] {grid[0][0], 0, 0}); // starting -> height, row, col
-        
-        int [][] visited = new int [n][n];
-        int [][] directions = {{-1, 0}, {0,-1}, {1, 0}, {0,1}};
-
-        while(!pq.isEmpty()){
-            int [] current = pq.poll();
-
-            int height = current[0];
-            int row = current[1];
-            int col = current[2];
-            time = Math.max(time, height);
-
-            if (row == n-1 && col == n-1) break;
-
-            // push your neighbours:
-            for (int [] direction : directions){
-                int newrow = row + direction[0];
-                int newcol = col + direction[1];
-
-                if (isValid(newrow, newcol, n) && visited[newrow][newcol] == 0){
-                    visited[newrow][newcol] = 1;
-                    pq.add(new int [] {grid[newrow][newcol], newrow, newcol});
-
-                }
-            }
-
-
-        }
-
-        return time;
-
-
-        
-
     }
 }
