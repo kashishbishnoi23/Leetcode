78faf0c98d26929b989b01728c78d82ef4d6f6fe
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-06 11:04:22 +0530

    Time: 10 ms (77.4%), Space: 45 MB (11.86%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -39,7 +39,8 @@ class Solution {
 
         // find mid -> see if u can reach (n-1, n-1) in mid time -> if yes -> then minimize the time => by doing end = mid-1
         // if not possible to reach in mid time => time badhana padega => start = mid + 1
-
+        
+        /*
         int max = 0;
 
         // O(N^2)
@@ -52,7 +53,8 @@ class Solution {
 
         int start = 0;
         int end = max;
-
+         
+        // max => N^2 -> O(N^2LOGN)
         while(start < end){
             int mid = start + (end - start )/2;
             // check if it is possible with mid:
@@ -70,5 +72,45 @@ class Solution {
         }
 
         return start;
+
+        */
+
+
+
+        // Solution 2 : Dijistra algorithm: at each point => move towards the minimum neighbour -> cuz it'll take minimum time to reach
+        int n = grid.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+
+        pq.offer(new int[]{grid[0][0], 0, 0}); // height, row, col
+        int time = 0;
+        boolean[][] visited = new boolean[n][n];
+        visited[0][0] = true;
+        int [][] directions = {{0,1}, {1,0}, {0,-1}, {-1, 0}};
+        while(!pq.isEmpty()){
+            int [] current = pq.poll();
+            int height = current[0];
+            int row = current[1];
+            int col = current[2];
+
+            // abhi I am standing at current elevation aur time hua ha t:
+            time = Math.max(time, height);
+            if (row == n-1 && col == n-1) return time;
+
+            // go to its neighbours => push them -> pq will give u the shortest elevation to pick:
+
+            for (int [] direction : directions){
+                int newrow = row + direction[0];
+                int newcol = col + direction[1];
+
+                if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < n &&  visited[newrow][newcol] == false){
+                    visited[newrow][newcol] = true;
+                    pq.offer(new int []{grid[newrow][newcol], newrow, newcol});
+                }
+            }
+
+        }
+
+        return time;
+
     }
 }
