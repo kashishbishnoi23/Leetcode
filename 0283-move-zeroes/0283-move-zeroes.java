66888fb60306b88b550fd1b5ab8b09f4ac8a8acc
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-06 11:33:42 +0530

    Time: 3 ms (19.38%), Space: 46.6 MB (14.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,39 @@
 class Solution {
+
+    private void swap(int first, int second, int[] nums){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<Integer> list = new ArrayList<>();
 
-        for (int num : nums){
-            if (num != 0) list.add(num);
-        }
+        // for (int num : nums){
+        //     if (num != 0) list.add(num);
+        // }
         
-        int index = 0;
+        // int index = 0;
+        // for (int i = 0; i < n; i++){
+        //     nums[i] = (index < list.size()) ? list.get(index) : 0;
+        //     index++;
+        // }
+        int lastZero = -1;
         for (int i = 0; i < n; i++){
-            nums[i] = (index < list.size()) ? list.get(index) : 0;
-            index++;
+            if (lastZero == -1 && nums[i] == 0){
+                lastZero = i;
+                continue;
+            }
+
+            if (nums[i] != 0 && lastZero != -1){
+                // swap and move lastZero:
+                swap(lastZero, i, nums);
+
+                while(lastZero <= i && nums[lastZero] != 0){
+                    lastZero ++;
+                }
+            }
+            
         }
 
         
