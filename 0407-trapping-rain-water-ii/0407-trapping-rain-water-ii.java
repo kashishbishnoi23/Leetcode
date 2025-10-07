6a505e1d1984dc587e33a40bebdc984be4618a14
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-07 18:36:40 +0530

    Time: 20 ms (40.59%), Space: 46.3 MB (73.12%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -1,67 +1,128 @@
 class Solution {
     public int trapRainWater(int[][] heightMap) {
+        // int n = heightMap.length;
+        // int m = heightMap[0].length;
+        // int water = 0;
+
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> 
+        //    Integer.compare(a[0], b[0])
+        // );
+
+        // boolean [][] visited = new boolean[n][m];
+
+        // // push the boundaries to the pq and mark them visited:
+
+        // // first and last row:
+        // for (int col = 0; col < m ; col++){
+        //     for (int row : new int[]{0,  n-1}){
+        //         visited[row][col] = true;
+        //         pq.offer(new int []{heightMap[row][col], row, col});
+        //     }
+        // }
+
+        // // first col and last col:
+        // for (int row = 0; row < n; row++){
+        //     for (int col : new int []{0, m-1}){
+        //         visited[row][col] = true;
+        //         pq.offer(new int[] {heightMap[row][col], row, col});
+        //     }
+        // }
+
+        // int [][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        // while(!pq.isEmpty()){
+        //     // pick the minimum boundary:
+        //     int[] minimum = pq.poll();
+        //     int height = minimum[0];
+        //     int row = minimum[1];
+        //     int col = minimum[2];
+
+        //     // get its neighbours: 
+        //     for (int [] direction : directions){
+        //         int neighbourRow = row + direction[0];
+        //         int neighbourCol = col + direction[1];
+
+                
+        //         if (neighbourRow >= 0 && neighbourRow < n && neighbourCol >= 0 && neighbourCol < m && visited[neighbourRow][neighbourCol] == false){
+        //             visited[neighbourRow][neighbourCol] = true;
+        //             int neighbourHeight = heightMap[neighbourRow][neighbourCol];
+        //             // calculate water:
+
+        //             // can it store water:
+        //             int waterStored = height - neighbourHeight;
+
+        //             if (waterStored > 0){
+        //                 water += waterStored;
+        //             }
+        //         pq.offer(new int[] {Math.max(height,neighbourHeight), neighbourRow, neighbourCol});
+        //         }
+
+        //     }
+        
+        // }
+
+        // return water;
+
         int n = heightMap.length;
         int m = heightMap[0].length;
-        int water = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> 
-           Integer.compare(a[0], b[0])
-        );
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0])); 
 
         boolean [][] visited = new boolean[n][m];
 
-        // push the boundaries to the pq and mark them visited:
-
-        // first and last row:
-        for (int col = 0; col < m ; col++){
-            for (int row : new int[]{0,  n-1}){
-                visited[row][col] = true;
-                pq.offer(new int []{heightMap[row][col], row, col});
+        // push the boundaries:
+        for (int i = 0; i < n; i++){
+            for (int col : new int[]{0, m-1}){
+                pq.offer(new int []{heightMap[i][col], i, col});
+                visited[i][col] = true;
             }
         }
 
-        // first col and last col:
-        for (int row = 0; row < n; row++){
-            for (int col : new int []{0, m-1}){
-                visited[row][col] = true;
-                pq.offer(new int[] {heightMap[row][col], row, col});
+        for (int j = 0; j < m; j++){
+            for (int row : new int[]{0, n-1}){
+                if (visited[row][j])continue;
+                pq.offer(new int [] {heightMap[row][j], row, j});
+                visited[row][j] = true;
             }
         }
+
+        int water = 0;
 
         int [][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-
         while(!pq.isEmpty()){
-            // pick the minimum boundary:
-            int[] minimum = pq.poll();
-            int height = minimum[0];
-            int row = minimum[1];
-            int col = minimum[2];
+            int [] current = pq.poll();
+            int height = current[0];
+            int row = current[1];
+            int col = current[2];
 
-            // get its neighbours: 
+            // this is the minimum boundary:
+            visited[row][col] = true;
+
             for (int [] direction : directions){
-                int neighbourRow = row + direction[0];
-                int neighbourCol = col + direction[1];
+                int newrow = row + direction[0];
+                int newcol = col + direction[1];
 
-                
-                if (neighbourRow >= 0 && neighbourRow < n && neighbourCol >= 0 && neighbourCol < m && visited[neighbourRow][neighbourCol] == false){
-                    visited[neighbourRow][neighbourCol] = true;
-                    int neighbourHeight = heightMap[neighbourRow][neighbourCol];
-                    // calculate water:
+                if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && visited[newrow][newcol] == false){
+                   visited[newrow][newcol] = true;
 
-                    // can it store water:
-                    int waterStored = height - neighbourHeight;
+                   if (heightMap[newrow][newcol] < height){
+                    water += height - heightMap[newrow][newcol];
+                   }
 
-                    if (waterStored > 0){
-                        water += waterStored;
-                    }
-                pq.offer(new int[] {Math.max(height,neighbourHeight), neighbourRow, neighbourCol});
+                   pq.offer(new int[]{Math.max(height, heightMap[newrow][newcol]), newrow, newcol});
+
                 }
-
             }
-        
-        }
 
+        }
+        
         return water;
+
+
+
+
+
+
 
 
 
