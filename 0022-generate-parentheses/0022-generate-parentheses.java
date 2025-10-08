132e85af945235379a152d5cd74bfd816faeca12
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-08 12:35:02 +0530

    Time: 0 ms (100%), Space: 43.4 MB (34.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,32 +1,28 @@
 class Solution {
-
     private void recursive(List<String> ans, StringBuilder sb, int n, int open, int close){
-        if (close == n){
+
+        if (open == close && close == n){
             ans.add(new String(sb));
             return;
         }
 
-        // add open:
+        if (close > open) return;
+        
+        // i can use open, if open < n:
         if (open < n){
             sb.append('(');
             recursive(ans, sb, n, open+1, close);
             sb.deleteCharAt(sb.length()-1);
         }
 
-        // add close
-        if (open > close){
-            sb.append(')');
-            recursive(ans, sb, n, open, close+1);
-            sb.deleteCharAt(sb.length()-1);
-        }
-        
-
+        sb.append(')');
+        recursive(ans, sb, n, open, close+1);
+        sb.deleteCharAt(sb.length()-1);
 
     }
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-
         recursive(ans, sb, n, 0, 0);
         return ans;
     }
