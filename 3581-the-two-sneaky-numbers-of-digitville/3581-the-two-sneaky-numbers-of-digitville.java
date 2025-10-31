828f60018719c040ab250b338ea0c0748299a25e
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-10-31 20:52:48 +0530

    Time: 1 ms (100%), Space: 45.5 MB (7.71%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,15 +1,37 @@
 class Solution {
+
+    private void swap(int first, int second, int[] nums){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
     public int[] getSneakyNumbers(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         int n = nums.length;
         int[] ans = new int[2];
         int index = 0;
 
-        for (int i = 1; i < n; i++){
-            if (nums[i] == nums[i-1]){
-                ans[index++] = nums[i];
+        // for (int i = 1; i < n; i++){
+        //     if (nums[i] == nums[i-1]){
+        //         ans[index++] = nums[i];
+        //     }
+        // }
+        // return ans;
+
+        for (int i = 0; i < n; i++){
+            // swap the number with the number at its correct position:
+            int j = i;
+            while(nums[nums[j]] != nums[j]){
+                swap(j, nums[j], nums);
             }
         }
+
+        ans[index++] = nums[n-2];
+        ans[index++] = nums[n-1];
         return ans;
+
+    
+
+
     }
 }
