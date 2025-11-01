30a22cad7aaf8672f7a3ea787f90700159e0ebb2
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-11-01 18:49:33 +0530

    Time: 1 ms (56.76%), Space: 46.6 MB (6.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,55 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-         
-        // using set :  TC : NlogN + N ,  SC = O(N)
-
-        // Set<Integer>st = new TreeSet<>(); // logN for each operation 
-
-        // for (int i = 0; i < nums.length; i++){
-        //     st.add(nums[i]);
-        // }
-        
-        // int j = 0;
-        // for (int num : st){
-        //      nums[j++] = num;
-        // }
-
-        // return st.size();
-
-        // two pointer approach:
-
-        // int size = nums.length;
-    
-        //  int i = 0;
-
-        // for (int j = 1; j < size; j++){
-             
-        //      if (nums[j] > nums[i]){
-        //         i++;
-        //         nums[i] = nums[j];
-        //      }
-
-        // }
-
-        // return i+1;
-
-
-
-        // use a set:
         int n = nums.length;
-
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int num : nums){
-            set.add(num);
+        int prev = nums[0];
+        int nextIndex = 1;
+     
+        for (int i = 1; i < n; i++){
+            int curr = nums[i];
+            if (curr > prev){
+                prev = curr;
+                nums[nextIndex] = curr;
+                nextIndex++;
+            }
         }
 
-        int i = 0;
-
-        for (int num : set){
-            nums[i++] = num;
-        }
-
-        return set.size();
-
+        return nextIndex;
+        
     }
 }
