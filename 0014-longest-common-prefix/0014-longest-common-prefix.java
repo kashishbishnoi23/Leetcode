d19a1125382a9f0470fe25258899ca026e1be5d1
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-11-01 19:15:42 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.33%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,20 +1,34 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length-1];
+        // Arrays.sort(strs);
+        // String first = strs[0];
+        // String last = strs[strs.length-1];
 
         // how many letters are same:
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < first.length(); i++){
-            char ch = first.charAt(i);
-            if (ch == last.charAt(i)){
-                sb.append(ch);
-            } else {
-                break;
+        // StringBuilder sb = new StringBuilder();
+        // for (int i = 0; i < first.length(); i++){
+        //     char ch = first.charAt(i);
+        //     if (ch == last.charAt(i)){
+        //         sb.append(ch);
+        //     } else {
+        //         break;
+        //     }
+        // }
+         
+        
+        // return sb.toString();
+
+        String prefix = strs[0];
+        // maan liya ki pehla string prefix hai:
+        for (int i = 0; i < strs.length; i++){
+
+            // jab tak current string prefix se start nahi krta -> keep on decrementing the prefix:
+            while(!strs[i].startsWith(prefix)){
+                prefix = prefix.substring(0, prefix.length()-1);
+                if (prefix.isEmpty()) return "";
             }
         }
 
-        return sb.toString();
+        return prefix;
     }
 }
