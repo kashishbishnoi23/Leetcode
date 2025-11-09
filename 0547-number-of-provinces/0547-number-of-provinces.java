1c4580c1123ce19aecb7e51335617d5e3d1a159f
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-11-09 15:53:54 +0530

    Time: 0 ms (100%), Space: 47.3 MB (75.2%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,51 +1,33 @@
 class Solution {
+    private void dfs(int node, boolean[] visited, int[][] isConnected){
+         
+         visited[node] = true;
 
-    private void dfs(int node, boolean [] visited, ArrayList<ArrayList<Integer>> adjList){
-        
-        // go to the nodes and visit them:
-        for (int neighbour : adjList.get(node)){
-            if (visited[neighbour] == false){
-                visited[neighbour] = true;
-                dfs(neighbour, visited, adjList);
+        // go to the neighbours:
+        for (int i = 0; i < isConnected.length; i++){
+            if (isConnected[node][i] == 1 && !visited[i]){
+                visited[i] = true;
+                dfs(i, visited, isConnected);
             }
         }
+
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
 
-        for(int i = 0; i < n; i++){
-            adjList.add(new ArrayList<>());
-        }
+        boolean[] visited = new boolean[n];
 
-        boolean [] visited = new boolean[n+1];
-
-        // creating an adjacency list:
+        int count = 0;
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (isConnected[i][j] == 1){
-                    adjList.get(i).add(j);
-                    adjList.get(j).add(i);
-                }
+            // mark the current node as visited:
+            if (!visited[i]){
+            visited[i] = true;
+            dfs(i, visited, isConnected);
+            count ++;
             }
         }
 
-        int comp = 0;
-
-        for (int i = 0; i < n; i++){
-            if (visited[i] == false){
-                comp++;
-                visited[i] = true;
-                dfs(i, visited, adjList);
-            }
-        }
-
-        return comp;
-
-
-
-
+        return count;
 
     }
 }
