540db7abb6901b39753a7f47f630a6cac5412c0b
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-11-22 11:43:37 +0530

    Time: 1044 ms (8%), Space: 67.9 MB (5.23%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,35 +1,7 @@
 class Solution {
-
-    private void recursive(String s, int index, HashSet<String> st, int length, StringBuilder temp){
-        if (index > s.length() || length > 3) return;
-
-        if (length == 3){
-            System.out.println(temp);
-            if (temp.charAt(0) == temp.charAt(2)){
-                st.add(new String(temp));
-            }
-            return;
-        } 
-
-        if (index == s.length()) return;
-
-        // pick:
-        temp.append(s.charAt(index));
-        recursive(s, index+1, st, length+1, temp);
-
-
-        if (temp.length() > 0){
-            temp.deleteCharAt(temp.length()-1);
-        }
-
-        // not pick:
-        recursive(s, index+1, st, length, temp);
-
-    }
     public int countPalindromicSubsequence(String s) {
         HashSet<String> st = new HashSet<>();
         
-        // StringBuilder temp = new StringBuilder();
 
         HashMap<Character, ArrayList<Integer>> hashing = new HashMap<>();
 
