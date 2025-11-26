149434ad7e477e3a8fc2fe51a159f3f513bc9fb9
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-11-26 15:07:34 +0530

    Time: 94 ms (36.97%), Space: 49.2 MB (23.28%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,140 +1,53 @@
 class Solution {
 
-    // class Pair{
-    //     String str;
-    //     int distance;
-
-    //     Pair(String str, int distance){
-    //         this.str = str;
-    //         this.distance = distance;
-    //     }
-    // }
-
-    class Pair{
+    class Node{
         String str;
-        int distance;
-        public Pair(String str, int distance){
+        int level;
+
+        public Node(String str, int level){
             this.str = str;
-            this.distance = distance;
+            this.level = level;
         }
     }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> st = new HashSet<>();
+        Set<String> done = new HashSet<>();
+    
 
-        if (!wordList.contains(endWord))return 0;
-        HashSet<String> set = new HashSet<>(wordList);
+        for (String word : wordList) st.add(word);
+            if (!st.contains(endWord)) return 0;
 
-        // level order traversal 
-
-        Queue<Pair> q = new LinkedList<>(); // store the strings at levels
-        Set<String> st = new HashSet<>(); // to avoid going back to an already done string
-
+        
         // start with beginWord:
-        q.offer(new Pair(beginWord, 1));
+        Queue<Node> q = new LinkedList<>();
 
-        while(!q.isEmpty()){     
+        q.offer(new Node(beginWord, 0));
+        done.add(beginWord);
 
-        int size = q.size();
+        while(!q.isEmpty()){
+            int size = q.size();
 
-        for (int i = 0; i < size; i++){
-            Pair pair = q.poll();
-            String str = pair.str;
-            int distance = pair.distance;
-            // System.out.println(str);
-            // System.out.println(distance);
+            for (int i = 0; i < size; i++){
+                Node front = q.poll();
+                String str = front.str;
+                int level = front.level;
+                if (str.equals(endWord)) return level+1;
+                 
 
-            if (str.equals(endWord)){
-                return distance;
-            }
-
-            st.add(str);
-
-            // go to the next word:
-            
-            for (int j = 0; j < str.toCharArray().length; j++){
-               char [] temp = str.toCharArray();
-
-               // change the ith letter with all a to z:
-               for (char ch = 'a'; ch <= 'z'; ch++){
-                 temp[j] = ch;
-                 String newstr = new String(temp);
-                 if (!st.contains(newstr) && set.contains(newstr)){
-                        st.add(newstr);
-                        q.offer(new Pair(newstr, distance+1));
-                 }
-               }
-
-               
-            }
-
+                for(int j = 0; j < str.length(); j++){
+                    StringBuilder sb = new StringBuilder(str);
+                    for (char ch = 'a'; ch <= 'z'; ch++){
+                        sb.setCharAt(j, ch);
+                        if (st.contains(sb.toString()) && !done.contains(sb.toString())){
+                            q.offer(new Node(sb.toString(), level+1));
+                            done.add(sb.toString());
+                        }
+                    }
+                }
             }
         }
 
         return 0;
 
-
-
-
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        
-        // Queue<Pair> q = new LinkedList<>();
-        // Set<String> st = new HashSet<>(wordList);
-
-        // if (!st.contains(endWord)){
-        //     return 0;
-        // }
-
-        // if (st.contains(beginWord)){
-        //     st.remove(beginWord);
-        // }
-
-        // q.add(new Pair(beginWord, 1));
-
-        // while(!q.isEmpty()){
-        //     Pair front = q.poll();
-        //     String str = front.str;
-        //     int distance = front.distance;
-
-        //     // System.out.println("string = " + str);
-        //     // System.out.println("distance = " + distance);
-
-        //     if (str.equals(endWord)){
-        //         return distance;
-        //     }
-            
-        //     char[] chars = str.toCharArray();
-
-        //     for (int i = 0; i < str.length(); i++){
-        //         char[] temp = Arrays.copyOf(chars, chars.length);
-        //         for (char alpha = 'a'; alpha <= 'z'; alpha++){
-        //             // replace ith character:
-        //             temp[i] = alpha;
-        //             String check = new String(temp);
-        //             // System.out.println(check);
-        //             if (st.contains(check)){
-
-        //                 st.remove(check);
-        //                 q.add(new Pair(check, distance+1));
-        //             }
-        //         }
-        //     }
-         
-        // }
-
-        // return 0;
     }
 }
