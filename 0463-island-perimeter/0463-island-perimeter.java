c101b29dba30a93e331a34e67f87ed94d6072e97
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-11-26 14:31:28 +0530

    Time: 54 ms (5.83%), Space: 47.9 MB (8.51%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,42 +1,74 @@
 class Solution {
 
-    int [][] directions = {{1,0}, {0,1}, {-1, 0}, {0,-1}};
-
-    private void dfs(int row, int col, int [][] visited, int [][] grid, int [] perimeter){
+    private void  dfs(int row, int col, int[][] grid, int[][] visited){
+        visited[row][col] = 1;
         int n = grid.length;
         int m = grid[0].length;
 
-        // mark it as visited and count how much it contributes:
-        for (int [] direction : directions){
-            int newrow = row + direction[0];
-            int newcol = col + direction[1];
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
 
-            if (newrow < 0 || newrow >= n || newcol < 0 || newcol >= m || grid[newrow][newcol] == 0){
-                perimeter[0]++;
-            }      
+        for (int [] dir : directions){
+            int newrow = row + dir[0];
+            int newcol = col + dir[1];
 
-            if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && visited[newrow][newcol] == 0 & grid[newrow][newcol] == 1){
+            if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && visited[newrow][newcol] == 0 && grid[newrow][newcol] == 1){
                 visited[newrow][newcol] = 1;
-                dfs(newrow, newcol, visited, grid, perimeter);
+               dfs(newrow, newcol, grid, visited);
+            }
+
+        }
+         
+        visited[row][col] = 0;
+        for (int[] dir : directions){
+            int newrow = row + dir[0];
+            int newcol = col + dir[1];
+            if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && grid[newrow][newcol] == 1){
+                visited[row][col] ++;
             }
         }
+
+
 
     }
     public int islandPerimeter(int[][] grid) {
-        int[] perimeter = {0};
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int [][] visited = new int [rows][cols];
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < cols; j++){
-                if (grid[i][j] == 1 && visited[i][j] == 0){
-                    // land found -> cover all the land components:
-                    visited[i][j] = 1;
-                    dfs(i, j, visited, grid, perimeter);
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] visited = new int[n][m];
+        int count = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (grid[i][j] == 1){
+                   count ++;
                 }
             }
         }
+        if (count == 1) return 4;
 
-        return perimeter[0];
+        boolean flag = false;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (grid[i][j] == 1){
+                    dfs(i, j, grid, visited);
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)break;
+
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                // System.out.print(visited[i][j] + " ");
+                if (visited[i][j] == 0) continue;
+               ans += 4 - visited[i][j];
+            }
+            System.out.println();
+        }
+
+        return ans;
+        
     }
 }
