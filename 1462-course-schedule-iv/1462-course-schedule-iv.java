97922c1be97aac460f6dbd22039d4256ea224191
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-05 22:48:00 +0530

    Time: 18 ms (80.46%), Space: 48 MB (87.86%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    private void dfs(int node, int start, ArrayList<ArrayList<Integer>> adj, boolean[][] isReachable){
+        if (isReachable[start][node] == true) return;
+        isReachable[start][node] = true;
+        
+        for (int neigh : adj.get(node)){
+            dfs(neigh, start, adj, isReachable);
+        }
+    }
+
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+
+        List<Boolean> ans = new ArrayList<>();
+        if (prerequisites.length == 0){
+            for (int i = 0; i < queries.length; i++){
+                ans.add(false);
+            }
+            return ans;
+        }
+
+        int n = numCourses;
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] p : prerequisites){
+            adj.get(p[0]).add(p[1]);
+        }
+
+        boolean[][] isReachable = new boolean[numCourses][numCourses];
+
+        for (int i = 0; i < numCourses; i++){
+            dfs(i, i, adj, isReachable);
+        }
+
+
+
+        for (int i = 0; i < queries.length; i++){
+            int a = queries[i][0];
+            int b = queries[i][1];
+
+            if (isReachable[a][b] == true){
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+
+        return ans;
+
+
+    }
+}
