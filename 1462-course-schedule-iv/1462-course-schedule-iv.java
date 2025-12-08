d8a8bda9585dc38db91afd98bfeec0d2248566e3
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-08 19:48:53 +0530

    Time: 28 ms (67.17%), Space: 51.3 MB (23.58%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -13,38 +13,81 @@ class Solution {
 
         // agar ek course A se mai course B pahuch paa rhi hu -> directly ya indirectly -> iska matlab muje B se pehle A course krna padega so B is dependent on A 
 
-        List<Boolean> ans = new ArrayList<>();
-        if (prerequisites.length == 0){
-            for (int i = 0; i < queries.length; i++){
-                ans.add(false);
-            }
-            return ans;
-        }
+        // List<Boolean> ans = new ArrayList<>();
+        // if (prerequisites.length == 0){
+        //     for (int i = 0; i < queries.length; i++){
+        //         ans.add(false);
+        //     }
+        //     return ans;
+        // }
 
-        int n = numCourses;
+        // int n = numCourses;
+
+        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // for (int i = 0; i < numCourses; i++){
+        //     adj.add(new ArrayList<>());
+        // }
+
+        // for (int[] p : prerequisites){
+        //     adj.get(p[0]).add(p[1]);
+        // }
+
+        // boolean[][] isReachable = new boolean[numCourses][numCourses];
+
+        // for (int i = 0; i < numCourses; i++){
+        //     dfs(i, i, adj, isReachable);
+        // }
+
+
+
+        // for (int i = 0; i < queries.length; i++){
+        //     int a = queries[i][0];
+        //     int b = queries[i][1];
+
+        //     if (isReachable[a][b] == true){
+        //         ans.add(true);
+        //     } else {
+        //         ans.add(false);
+        //     }
+        // }
+
+        // return ans;
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++){
+        for(int i = 0; i < numCourses; i++){
             adj.add(new ArrayList<>());
         }
 
-        for (int[] p : prerequisites){
-            adj.get(p[0]).add(p[1]);
-        }
-
-        boolean[][] isReachable = new boolean[numCourses][numCourses];
+        boolean[][]isReachable = new boolean[numCourses][numCourses];
 
         for (int i = 0; i < numCourses; i++){
-            dfs(i, i, adj, isReachable);
+            isReachable[i][i] = true;
         }
 
+        for(int[] pre : prerequisites){
+            int a = pre[0];
+            int b = pre[1];
 
+            adj.get(a).add(b);
+            isReachable[a][b] = true;
+        }
 
-        for (int i = 0; i < queries.length; i++){
-            int a = queries[i][0];
-            int b = queries[i][1];
+        for (int k = 0; k < numCourses; k++){
+            for (int i = 0; i < numCourses; i++){
+                for (int j = 0; j < numCourses; j++){
+                    if (isReachable[i][j] == false){
+                        if (isReachable[i][k] && isReachable[k][j]){
+                            isReachable[i][j] = true;
+                        }
+                    }
+                }
+            }
+        }
 
-            if (isReachable[a][b] == true){
+        ArrayList<Boolean> ans = new ArrayList<>();
+
+        for (int[] q : queries){
+            if (isReachable[q[0]][q[1]]){
                 ans.add(true);
             } else {
                 ans.add(false);
