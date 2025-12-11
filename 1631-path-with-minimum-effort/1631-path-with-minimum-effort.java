19dd50ebb62b8e9c8184cc081667558b0af94359
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-11 17:03:25 +0530

    Time: 41 ms (64.81%), Space: 48 MB (23.53%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,31 +1,27 @@
 class Solution {
 
-    class Pair{
+    class Node{
         int row;
         int col;
-        int maxeffort;
-
-        Pair( int maxeffort, int row, int col){
-            this.maxeffort = maxeffort;
+        int maxe;
+        public Node(int row, int col, int maxe){
             this.row = row;
             this.col = col;
-            this.maxeffort = maxeffort;
+            this.maxe = maxe;
         }
     }
-
-    private static int [][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
     public int minimumEffortPath(int[][] heights) {
+
         int n = heights.length;
         int m = heights[0].length;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.maxe, b.maxe));
 
-        int[][] efforts = new int [n][m];
-        int ans = -1;
 
-        PriorityQueue<Pair>q = new PriorityQueue<>((x, y) -> x.maxeffort - y.maxeffort);
+        pq.offer(new Node(0, 0, 0));
 
-        q.add(new Pair(0,0,0));
+        int[][] directions = {{0,1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        
+        int[][] efforts = new int[n][m];
 
         for (int i = 0; i < n; i++){
             Arrays.fill(efforts[i], Integer.MAX_VALUE);
@@ -33,37 +29,34 @@ class Solution {
 
         efforts[0][0] = 0;
 
-        while(!q.isEmpty()){
-            Pair pair = q.poll();
-            int effort = pair.maxeffort;
-            int row = pair.row;
-            int col = pair.col;
+        while(!pq.isEmpty()){
+            Node front = pq.poll();
+            int row = front.row;
+            int col = front.col;
+            int maxe = front.maxe;
 
-            // System.out.println(effort + " " + row + " " + col);
+            if (row == n-1 && col == m-1) return maxe;
 
-            if (row == n-1 && col == m-1) return effort; 
-
-            // go to all the 4 directions:
-            for (int[] direction : directions){
-                int nrow = row + direction[0];
-                int ncol = col + direction[1];
+            for(int[] dir : directions){
+                int nrow = row + dir[0];
+                int ncol = col + dir[1];
 
                 if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
-                    int neweffort = Math.abs(heights[row][col] - heights[nrow][ncol]);
-                    if (efforts[nrow][ncol] > neweffort){
+                    int neweffort = Math.max(maxe, Math.abs(heights[row][col] - heights[nrow][ncol]));
+
+                    if (neweffort < efforts[nrow][ncol]){
                         efforts[nrow][ncol] = neweffort;
-                        q.add(new Pair(Math.max(effort, neweffort), nrow, ncol));
+                        pq.offer(new Node(nrow, ncol, neweffort));
                     }
+
+
                 }
             }
+
+
+
         }
 
-        return ans;
-
-        
-
-         
-
-
+        return -1;
     }
 }
