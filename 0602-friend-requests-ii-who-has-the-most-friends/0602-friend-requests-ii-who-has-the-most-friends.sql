275a58c4dd137ece5beab358fb5c7e22b4e73b75
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-18 20:51:41 +0530

    Time: 2549 ms (5.03%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -10,25 +10,37 @@
 -- LIMIT 1;
 
 
-SELECT id, num
+SELECT id, COUNT(*) AS num
 FROM (
-    SELECT id, COUNT(*) AS num
-    FROM (
-        SELECT requester_id AS id FROM RequestAccepted
-        UNION ALL
-        SELECT accepter_id  AS id FROM RequestAccepted
-    ) t
-    GROUP BY id
-) c
-WHERE num = (
-    SELECT MAX(num)
-    FROM (
-        SELECT id, COUNT(*) AS num
-        FROM (
-            SELECT requester_id AS id FROM RequestAccepted
-            UNION ALL
-            SELECT accepter_id  AS id FROM RequestAccepted
-        ) t2
-        GROUP BY id
-    ) m
-);
+SELECT requester_id as id  from RequestAccepted 
+UNION ALL
+SELECT accepter_id as id  from RequestAccepted
+) t 
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
+;
+
+
+-- SELECT id, num
+-- FROM (
+--     SELECT id, COUNT(*) AS num
+--     FROM (
+--         SELECT requester_id AS id FROM RequestAccepted
+--         UNION ALL
+--         SELECT accepter_id  AS id FROM RequestAccepted
+--     ) t
+--     GROUP BY id
+-- ) c
+-- WHERE num = (
+--     SELECT MAX(num)
+--     FROM (
+--         SELECT id, COUNT(*) AS num
+--         FROM (
+--             SELECT requester_id AS id FROM RequestAccepted
+--             UNION ALL
+--             SELECT accepter_id  AS id FROM RequestAccepted
+--         ) t2
+--         GROUP BY id
+--     ) m
+-- );
