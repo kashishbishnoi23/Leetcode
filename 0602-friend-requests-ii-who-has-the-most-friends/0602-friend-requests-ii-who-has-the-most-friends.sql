e1444cc7aaa06d804cab3f9da75777d56bb7ccd1
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-18 20:45:52 +0530

    Time: 304 ms (40.4%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,10 +1,34 @@
 # Write your MySQL query statement below
-SELECT id, COUNT(*) AS num
+-- SELECT id, COUNT(*) AS num
+-- FROM (
+--     SELECT requester_id AS id FROM RequestAccepted
+--     UNION ALL
+--     SELECT accepter_id  AS id FROM RequestAccepted
+-- ) t
+-- GROUP BY id
+-- ORDER BY num DESC
+-- LIMIT 1;
+
+
+SELECT id, num
 FROM (
-    SELECT requester_id AS id FROM RequestAccepted
-    UNION ALL
-    SELECT accepter_id  AS id FROM RequestAccepted
-) t
-GROUP BY id
-ORDER BY num DESC
-LIMIT 1;
+    SELECT id, COUNT(*) AS num
+    FROM (
+        SELECT requester_id AS id FROM RequestAccepted
+        UNION ALL
+        SELECT accepter_id  AS id FROM RequestAccepted
+    ) t
+    GROUP BY id
+) c
+WHERE num = (
+    SELECT MAX(num)
+    FROM (
+        SELECT id, COUNT(*) AS num
+        FROM (
+            SELECT requester_id AS id FROM RequestAccepted
+            UNION ALL
+            SELECT accepter_id  AS id FROM RequestAccepted
+        ) t2
+        GROUP BY id
+    ) m
+);
