0c03ec7a5dc2fd7f764d8b5c064370c3a7f82804
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-18 20:59:43 +0530

    Time: 315 ms (26.52%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+
 -- SELECT id, COUNT(*) AS num
 -- FROM (
 --     SELECT requester_id AS id FROM RequestAccepted
@@ -10,45 +11,33 @@
 -- LIMIT 1;
 
 
-SELECT id, COUNT(*) AS num
-FROM (
-SELECT requester_id as id  from RequestAccepted 
-UNION ALL
-SELECT accepter_id as id  from RequestAccepted
-) t 
-GROUP BY id having num = (
-    SELECT MAX(num) 
-    FROM (
-        SELECT id, COUNT(*) AS num
-        FROM (
-            SELECT requester_id as id from RequestAccepted
-            UNION ALL
-            SELECT accepter_id as id from RequestAccepted
-        ) t2 
-        GROUP BY id
-    )m
-);
-
-
--- SELECT id, num
+-- SELECT id, COUNT(*) AS num
 -- FROM (
---     SELECT id, COUNT(*) AS num
---     FROM (
---         SELECT requester_id AS id FROM RequestAccepted
---         UNION ALL
---         SELECT accepter_id  AS id FROM RequestAccepted
---     ) t
---     GROUP BY id
--- ) c
--- WHERE num = (
---     SELECT MAX(num)
+-- SELECT requester_id as id  from RequestAccepted 
+-- UNION ALL
+-- SELECT accepter_id as id  from RequestAccepted
+-- ) t 
+-- GROUP BY id having num = (
+--     SELECT MAX(num) 
 --     FROM (
 --         SELECT id, COUNT(*) AS num
 --         FROM (
---             SELECT requester_id AS id FROM RequestAccepted
+--             SELECT requester_id as id from RequestAccepted
 --             UNION ALL
---             SELECT accepter_id  AS id FROM RequestAccepted
---         ) t2
+--             SELECT accepter_id as id from RequestAccepted
+--         ) t2 
 --         GROUP BY id
---     ) m
+--     )m
 -- );
+
+
+SELECT id, COUNT(*) as num 
+FROM
+(
+SELECT requester_id as id from RequestAccepted 
+UNION ALL
+SELECT accepter_id as id from RequestAccepted
+) t 
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
