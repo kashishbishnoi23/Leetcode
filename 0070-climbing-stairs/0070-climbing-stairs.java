2e308388048b0206ba9d5bb02555e19736715bf2
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-22 00:02:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (8.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -18,18 +18,26 @@ class Solution {
     }
 
     public int climbStairs(int n) {
+        if (n == 1) return 1;
         
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
         // return recursion(n, dp);
 
-        dp[0] = 1;
-        dp[1] = 1;
+        // dp[0] = 1;
+        // dp[1] = 1;
+        int first = 1;
+        int second = 1;
+        int third = 0;
 
         for (int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            // dp[i] = dp[i-1] + dp[i-2];
+            third = first + second;
+
+            first = second;
+            second = third;
         }
 
-        return dp[n];
+        return third;
     }
 }
