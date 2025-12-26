a3c2dbe1fdbc6c18e926b4ce6f246b907becf323
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-26 10:53:12 +0530

    Time: 3 ms (83.61%), Space: 45.4 MB (73.96%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,34 +1,35 @@
 class Solution {
-    private int recursion(int row, int col, List<List<Integer>>triangle, int[][] dp){
-        int n = triangle.size();
+    // private int recursion(int row, int col, List<List<Integer>>triangle, int[][] dp){
+    //     int n = triangle.size();
         
-        if (row >= n ) return Integer.MAX_VALUE;
-        int m = triangle.get(row).size();
-        if (col >= m) return Integer.MAX_VALUE;
-        int curr = triangle.get(row).get(col);
+    //     if (row >= n ) return Integer.MAX_VALUE;
+    //     int m = triangle.get(row).size();
+    //     if (col >= m) return Integer.MAX_VALUE;
+    //     int curr = triangle.get(row).get(col);
 
-        if (row == n-1) return curr;
+    //     if (row == n-1) return curr;
 
-        if (dp[row][col] != Integer.MAX_VALUE) return dp[row][col];
+    //     if (dp[row][col] != Integer.MAX_VALUE) return dp[row][col];
 
-        // take jth of next:
-        int first = Integer.MAX_VALUE;
+    //     // take jth of next:
+    //     int first = Integer.MAX_VALUE;
 
-        int next1 = recursion(row+1, col, triangle, dp);
-        if (next1 != Integer.MAX_VALUE){
-            first = curr + next1;
-        }
+    //     int next1 = recursion(row+1, col, triangle, dp);
+    //     if (next1 != Integer.MAX_VALUE){
+    //         first = curr + next1;
+    //     }
 
-        // take j+1 th of next:
-        int second = Integer.MAX_VALUE;
-        int next2 = recursion(row+1, col+1, triangle, dp);
+    //     // take j+1 th of next:
+    //     int second = Integer.MAX_VALUE;
+    //     int next2 = recursion(row+1, col+1, triangle, dp);
 
-        if (next2 != Integer.MAX_VALUE){
-            second = curr + next2;
-        }
+    //     if (next2 != Integer.MAX_VALUE){
+    //         second = curr + next2;
+    //     }
 
-        return dp[row][col] = Math.min(first, second);
-    }
+    //     return dp[row][col] = Math.min(first, second);
+    // }
+
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         // int[][] dp = new int[n][n];
@@ -37,7 +38,7 @@ class Solution {
         // }
         // return recursion(0, 0, triangle, dp);
 
-        int[] dp = new int[n];
+        // int[] dp = new int[n];
 
         int[] next = new int[n];
         
