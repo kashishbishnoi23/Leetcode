6356290d6ce33c973dc942a8a7cf2c56463bc842
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-26 09:27:28 +0530

    Time: 64 ms (50.65%), Space: 90.8 MB (54.59%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,40 +1,83 @@
 class Solution {
 
-    int MOD = (int) 1e9 + 7;
+    static final int MOD = 1000000007;
 
-    private int recursive(int row, int col, int[][] grid, int k , int mod, int[][][] dp){
+    private int recursion(int[][] grid, int row, int col, int k, int sum){
         int n = grid.length;
         int m = grid[0].length;
+        
         if (row >= n || col >= m) return 0;
-            int newmod = (mod + grid[row][col]) % k;
+          // check for the curr:
+        int rem = grid[row][col] % k;
+        int next = (sum + rem) % k;
+
         if (row == n-1 && col == m-1){
-            // System.out.println(sum + grid[row][col]);
-            return newmod == 0 ? 1 : 0;
-           
-        }
-    
+            if (next == 0) return 1;
+            return 0;
+        } 
 
-        if (dp[row][col][newmod] != -1){
-            return dp[row][col][newmod];
-        }
-        // go right:
-        int right = recursive(row, col + 1, grid, k, newmod, dp);
-        // go down
-        int down = recursive(row+1, col, grid, k, newmod, dp);
+        int down = recursion(grid, row+1, col, k, next);
 
-        return dp[row][col][newmod] = ((right % MOD) + (down % MOD)) % MOD;
+        int right = recursion(grid, row, col+1, k, next);
+
+        return (down + right) % MOD;
+
     }
     public int numberOfPaths(int[][] grid, int k) {
+        // return recursion(grid, 0, 0, k, 0);
         int n = grid.length;
         int m = grid[0].length;
 
         int[][][] dp = new int[n][m][k];
-        for(int i = 0; i < n; i++){
+
+        dp[0][0][grid[0][0] % k] = 1;
+
+        // tabulation:
+        for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
-                Arrays.fill(dp[i][j], -1);
+                if (i == 0 && j == 0) continue;
+                int rem = grid[i][j] % k; 
+                for (int r = 0; r < k; r++){
+
+                    // take from top:
+
+                    // top:
+                    if (i > 0){
+                        if (dp[i-1][j][r] > 0){
+                            int prev = r;
+                            int newrem = (prev + rem) % k;
+                            dp[i][j][newrem] = (dp[i][j][newrem] + dp[i-1][j][r]) % MOD; 
+                        }
+                    }
+
+                    // take from left:
+                    if (j > 0){
+                        if (dp[i][j-1][r] > 0){
+                            int prev = r;
+                            int newrem = (r + rem) % k;
+                            dp[i][j][newrem] = (dp[i][j][newrem] + dp[i][j-1][r]) % MOD;
+                        }
+                    }
+
+
+                }
             }
         }
-         
-        return recursive(0, 0, grid, k, 0, dp);
+
+        // int count = 0;
+        // for (int i = 0; i < n; i++){
+        //     for (int j = 0; j < m; j++){
+        //         System.out.println(i + " " + j);
+        //         for (int r = 0; r < k; r++){
+        //             System.out.print(dp[i][j][r] + " ");
+        //         }
+        //         System.out.println();
+        //     }
+        // }
+
+        return dp[n-1][m-1][0];
+
+
+
     }
 }
