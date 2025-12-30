6a9f55ca63bdd368aaf396ed22e00887fddf25fb
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-30 19:42:21 +0530

    Time: 194 ms (63.66%), Space: 50.7 MB (50.87%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -30,14 +30,63 @@ class Solution {
 
         int n = nums.size();
 
+
+
         int[][] dp = new int[n][target+1];
 
         for (int i = 0; i < n; i++){
             Arrays.fill(dp[i], -1);
         }
 
-        int ans  = recursive(nums, target, 0, dp);
-        return ans == Integer.MIN_VALUE ? -1 : ans;
+        for (int i = 0; i < n; i++){
+            dp[i][0] = 0; // har index pe 0 target banane k liye 0 hi elements chahiye
+        }
+
+        // 0th index wale element ko 0th index tak banane k liye max 1 hi element chahiye
+        // dp[0][nums.get(0)] = 1;
+
+        // int ans  = recursive(nums, target, 0, dp);
+        // return ans == Integer.MIN_VALUE ? -1 : ans;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 1; j <= target; j++){
+                if (i == 0){
+                    // I can only take this element , there are no prev elements:
+                    if (j == nums.get(0)){
+                        dp[0][j] = 1;
+                    }
+                    continue;
+                }
+
+                if (j < nums.get(i)){
+                    dp[i][j] = dp[i-1][j];
+                    continue; 
+                } 
+
+                
+                int nottake = dp[i-1][j]; // pichle index tak is sum ko banane k liye kitne elements lage
+
+                // int take = 1 + dp[i-1][j-nums[i]];
+                int take = -1;
+                int prev = dp[i-1][j-nums.get(i)];
+                if (prev != -1){
+                   take = 1 + prev;
+                }
+                // agar is element ko lenge to 1 element iska add hoga + bache hue sum ko banane me pichle index pe kitne elements lage -> for example, j is 4, and nums[i] is 1 -> to 1 element 1 khud hua plus pichle index pe 3 banane me max kitne elements use hue 
+
+                dp[i][j] = Math.max(nottake, take);
+
+            }
+        }
+
+        // for (int i = 0; i < n; i++){
+        //     for (int j = 0; j <= target; j++){
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        return dp[n-1][target];
 
     }
 }
