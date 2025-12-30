f29453a38f3430a502fc68d68768fc3389e4b062
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2025-12-30 10:24:21 +0530

    Time: 1269 ms (5.01%), Space: 47 MB (29.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -25,14 +25,69 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         // return recursion(coins, 0, amount) == Integer.MAX_VALUE
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        for (int i = 0; i < n; i++){
-            Arrays.fill(dp[i], -1);
+        // int[][] dp = new int[n][amount+1];
+        // for (int i = 0; i < n; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // int ans = recursion(coins, 0, amount, dp);
+
+
+
+        // return ans == Integer.MAX_VALUE ? -1 : ans;
+
+        int[] prev = new int[amount+1];
+        Arrays.fill(prev, Integer.MAX_VALUE);
+
+        prev[0] = 0;
+
+        for (int i = 1; i <= amount; i++){
+            if (coins[0] > i) continue;
+            int rem = i-coins[0]; // remaining amt ko banane me kitne coins lagenge
+
+            if (prev[rem] != Integer.MAX_VALUE){
+                prev[i] = 1 + prev[rem];
+            }
+
         }
-        int ans = recursion(coins, 0, amount, dp);
+
+     
+
+
+        for (int i = 1; i < n; i++){
+
+            int[] curr = new int[amount+1];
+            Arrays.fill(curr, Integer.MAX_VALUE);
+            curr[0] = 0;
+            for (int amt = 1; amt <= amount; amt++){
+                int coin = coins[i];
+                // mere paas ye coin hai aur muje amt banana hai
+                if (coin > amt){
+                    curr[amt] = prev[amt];
+                    continue;
+                }
+
+                // agar coin amt se kam hua tabhi ham coin se amt bana skte hain:
+                int nottake = prev[amt];
+                int take = Integer.MAX_VALUE;
+
+                int rem = amt - coin; // remaining coins 
+                if (curr[rem] != Integer.MAX_VALUE){
+                    take = 1 + curr[rem];
+                }
+
+                curr[amt] = Math.min(take, nottake);
+            }
+            prev = curr;
+    }
+
+        for (int am : prev){
+            System.out.println(am);
+        }
+
+        return prev[amount] == Integer.MAX_VALUE ? -1 : prev[amount];
+       
 
 
 
-        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
