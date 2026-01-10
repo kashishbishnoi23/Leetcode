c2b48f7134b6996613882718977f0215f7eef20a
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-10 13:25:32 +0530

    Time: 20 ms (78.46%), Space: 53.8 MB (79.92%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -16,12 +16,29 @@ class Solution {
         // T.C in simple recursion = 2^ max(m, n)
         int m = text1.length();
         int n = text2.length();
-        int[][] dp = new int[m][n];
+        // int[][] dp = new int[m][n];
 
-        for (int i = 0; i < m; i++){
-            Arrays.fill(dp[i], -1);
+        // for (int i = 0; i < m; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+
+        // return recursion(text1, text2, 0, 0, dp);
+
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
 
-        return recursion(text1, text2, 0, 0, dp);
+        return dp[m][n];
+
+
+
     }
 }
