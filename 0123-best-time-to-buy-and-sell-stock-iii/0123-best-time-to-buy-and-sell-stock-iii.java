fd0111e73cf2247f9695001eb3c3340c845390b0
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-10 23:13:52 +0530

    Time: 52 ms (40.77%), Space: 118.2 MB (28.06%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -35,13 +35,56 @@ class Solution {
     public int maxProfit(int[] prices) {
 
         int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for(int i = 0; i < n; i++){
-            for (int j = 0; j < 2; j++){
-                Arrays.fill(dp[i][j], -1);
+        int[][][] dp = new int[n+1][2][3];
+        // for(int i = 0; i < n; i++){
+        //     for (int j = 0; j < 2; j++){
+        //         Arrays.fill(dp[i][j], -1);
+        //     }
+        // }
+        // return recursion(prices, 0, 0, 2, dp);
+
+        // for k = 0, indx and state can be anything ans is 0:
+        // for (int i = 0; i <= n; i++){
+        //     for (int state = 0; state <= 1; state++){
+        //         // if (k == 0) dp[i][state][k] = 0;
+        //         dp[i][state][0] = 0;
+        //     }
+        // }
+
+        // // for indx n , -> 0
+        // for (int state = 0; state <= 1; state++){
+        //     for (int k = 0; k <= 2; k++){
+        //         dp[n][state][k] = 0;
+        //     }
+        // }
+
+        for (int i = n-1; i >= 0; i--){
+            for (int state = 0; state <= 1; state++){
+                for (int k = 1; k <= 2; k++){
+                    // for k = 0 its already 0 , so start from 1
+                    int profit = 0;
+                    if (state == 0){
+                        int buy = -prices[i] + dp[i+1][1][k];
+                        int notbuy = dp[i+1][0][k];
+                        profit = Math.max(buy, notbuy);
+
+                    } else {
+                        int sell = prices[i] + dp[i+1][0][k-1];
+                        int notsell = dp[i+1][1][k];
+
+                        profit = Math.max(sell, notsell);
+
+                    }
+
+                    dp[i][state][k] = profit;
+                }
             }
         }
-        return recursion(prices, 0, 0, 2, dp);
+
+        return dp[0][0][2];
+
+
+
 
     }
 }
