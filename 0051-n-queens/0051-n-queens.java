d1a38a842552dd2442dedba25e73c94c7bbb50a4
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-13 21:05:37 +0530

    Time: 5 ms (27.98%), Space: 47 MB (10.23%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,63 +1,63 @@
 class Solution {
-     
-    private boolean isValid(int n, int row, int index, List<StringBuilder> temp){
-        if (row == 0) return true;
-        // agar upar wali row ka element is row me clash na kre to valid, otherwise not valid:
 
-        boolean above = false;
+    private void recursive(int n,int[][] filled, int row, List<List<String>> ans){
 
-        for (int i = 0; i < row; i++){
-            if (temp.get(i).charAt(index) == 'Q') return false;
-        }
-        
-        int col = 1;
-        for (int j = row-1; j >= 0; j--){
-            if (index-col >= 0 && temp.get(j).charAt(index-col) == 'Q') return false;
-
-            if (index+ col < n && temp.get(j).charAt(index+col) == 'Q') return false;
-            col++;
-        }
-
-        return true;      
-    }
-    private boolean helper(int n, int row, List<List<String>> ans, List<StringBuilder> temp){
         if (row == n){
-            // ans.add(temp.toString());
-            ArrayList<String> newstr = new ArrayList<>();
-            for (StringBuilder sb : temp){
-                newstr.add(sb.toString());
+            List<String> path = new ArrayList<>();
+            for (int i = 0; i < n; i++){
+                StringBuilder temp = new StringBuilder();
+                for (int j = 0; j < n; j++){
+                     if (filled[i][j] == 0){
+                        temp.append(".");
+                     } else {
+                        temp.append("Q");
+                     }
+                }
+                path.add(temp.toString());
             }
-            ans.add(newstr);
-            return true;
+            ans.add(path);
+            return;
+
+        }
+         
+         // fill the current row and current col , then move to the next row:
+         // is it okay to fill the current col:
+         for (int col = 0; col < n; col++){
+            if (isValid(filled, row, col, n)){
+                filled[row][col] = 1;
+                // go to the next row:
+                recursive(n, filled, row+1, ans);
+                filled[row][col] = 0; // try another col and go again
+
+            }
+         }
+
+
+    }
+
+    private boolean isValid(int[][] filled, int row, int col, int n){
+        for (int i = 0; i < row; i++){
+            int diff = row-i;
+            int[] temp = {-diff, 0, diff};
+
+            for (int d : temp){
+                int newcol = col + d;
+                if (newcol >= 0 && newcol < n){
+                    if (filled[i][newcol] == 1) return false;
+                }
+
+            }
         }
 
-       // pick current index:
-       for (int i = 0; i < n; i++){
-          if (isValid(n, row, i, temp)){
-              // add queen at current index and move on:
-              temp.get(row).setCharAt(i, 'Q');
-
-                boolean next = helper(n, row+1, ans, temp);
-                // helper(n, row+1, ans, temp);
-                temp.get(row).setCharAt(i, '.');
-                // if (next == true) return true;
-          }
-       }
-       return false;
+        return true;
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        List<StringBuilder> temp = new ArrayList<>();
+        // List<String> temp = new ArrayList<>();
 
-        // n number of rows:
-        for (int i = 0; i < n; i++){
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++){
-                sb.append('.');
-            }
-            temp.add(sb);
-        }
-        helper(n, 0, ans,temp);
+        int[][] filled = new int[n][n];
+
+        recursive(n, filled, 0, ans);
 
         return ans;
     }
