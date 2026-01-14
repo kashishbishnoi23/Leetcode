2e979bc9c162093baffb505ee2917657bc9746e0
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-14 11:32:13 +0530

    Time: 6 ms (47.79%), Space: 46.7 MB (70.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,16 +4,26 @@ class Solution {
 
         int[] ans = new int[2];
 
+        // for (int i = 0; i < n; i++){
+        //     int a = nums[i];
+        //     int req = target-a;
+        //     for (int j = i+1; j < n; j++){
+        //         if (nums[j] == req){
+        //             ans[0] = i;
+        //             ans[1] = j;
+        //             return ans;
+        //         }
+        //     }
+        // }
+
+        HashMap<Integer, Integer> hashing = new HashMap<>();
+
         for (int i = 0; i < n; i++){
-            int a = nums[i];
-            int req = target-a;
-            for (int j = i+1; j < n; j++){
-                if (nums[j] == req){
-                    ans[0] = i;
-                    ans[1] = j;
-                    return ans;
-                }
+            if (hashing.containsKey(target-nums[i])){
+                ans[0] = i;
+                ans[1] = hashing.get(target-nums[i]);
             }
+            hashing.put(nums[i], i);
         }
         return ans;
     }
