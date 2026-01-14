aa91a7abde5e12260c1d7ccce78be22beb548f64
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-14 13:19:41 +0530

    Time: 0 ms (100%), Space: 44.7 MB (36.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,53 +2,50 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
 
-        // start from back -> find the point where the array is not in descending order anymore:
-       int i = n-1;
-       while(i > 0 && nums[i] <= nums[i-1]){
-          i--;
-       } 
+        // peeche se start kro -> agar ascending jata jara hai -> stop where it breaks:
 
-       if (i == 0){
-          // reverse the array and return:
-          int start = 0;
-          int end = n-1;
-          while(start < end){
+        // eg, 354321. ab last se start kru agar mai -> 21 -> 21 sabse bada number hai jo 2 aur 1 se banega -> can't make it more bigger
+        // 321 -> cant make it bigger either
+        // so we'll stop at first index - 3 
+
+        int b_indx = -1;
+
+        for (int j = n-2; j >= 0; j--){
+            if (nums[j] < nums[j+1]){
+                b_indx = j;
+                break;
+            }
+        }
+
+        // System.out.println("break index = " + b_indx);
+
+        // break_indx se just badi digit dhundho:
+
+        if (b_indx >= 0){
+        for (int i = n-1; i > b_indx; i--){
+            // System.out.println(nums[i] + " " + nums[b_indx]);
+            if (nums[i] > nums[b_indx]){
+                // swap with b_indx:
+                int temp = nums[i];
+                nums[i] = nums[b_indx];
+                nums[b_indx] = temp;
+                break;
+            }
+        }
+        }
+
+  
+
+        // reverse the array from b_indx + 1 to n-1:
+        int start = b_indx + 1;
+        int end = n-1;
+
+        while(start <= end){
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
             start++;
             end--;
-          }
-          return;
-       }
-
-       int replacement = -1;
-
-       int temp = nums[i-1];
-       for (int l = n-1; l >= i; l--){
-           if (nums[l] > temp){
-            replacement = l;
-            break;
-           }
-       }
-
-       nums[i-1] = nums[replacement];
-       nums[replacement] = temp;
-
-       // ab i se n-1 ko ascending order me arrange krdo:
-       int [] temparr = new int [n-i];
-    //    if (temparr.length <= 1) return;
-       int indx = 0;
-       for (int j = i; j < n; j++){
-        temparr[indx++] = nums[j];
-       }
-
-       Arrays.sort(temparr);
-       indx = 0;
-       for (int j = i; j < n; j++){
-        nums[j] = temparr[indx++];
-       }
-
-   
+        }
     }
 }
