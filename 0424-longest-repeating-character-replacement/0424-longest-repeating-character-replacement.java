f65ebd7099e3ee2d12d8c71dc9dda53f60e69589
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-15 10:07:29 +0530

    Time: 19 ms (36.51%), Space: 46.9 MB (28.74%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,27 +1,26 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int length = s.length();
-        int ans = 1;
-        int maxFreq = 0;
-        HashMap<Character, Integer> hashing = new HashMap<>();
+        int n = s.length();
         int left = 0;
+        int right = 0;
+        
+        // left se leke right tak ek window banegi -> isme jis character ki frequency maximum hai + k (bache hue characters jinko ham change krke max frequency wale character me convert kr skte hain) -> agar ye dono milke window size k equal aa jaye -> valid window else its an invalid window
+        int maxFreq = 0;
+        int ans = 1;
+        HashMap<Character, Integer> hashing = new HashMap<>();
+        while(right < n){
+             char ch = s.charAt(right);
+             maxFreq =  Math.max(maxFreq, hashing.merge(ch, 1, Integer:: sum));
 
-        for (int right = 0; right < length; right++){
-            char ch = s.charAt(right);
-            maxFreq = Math.max(maxFreq, hashing.merge(ch, 1, Integer::sum));
+             while(maxFreq + k < right-left+1){
+                  hashing.merge(s.charAt(left), -1, Integer:: sum);
+                  left++;
+             }
 
-            while(right-left+1 - maxFreq > k){
-                hashing.merge(s.charAt(left), -1, Integer::sum);
-                left++;
-            }
-
-            ans = Math.max(ans, right-left+1);
-
-
+             ans = Math.max(ans, right-left+1);
+             right++;
         }
 
         return ans;
-
-        
     }
 }
