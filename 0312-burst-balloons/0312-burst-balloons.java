fbea4d0082a73a14caa5de3c8996988bba851a52
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-21 17:08:23 +0530

    Time: 40 ms (94.33%), Space: 44.5 MB (56.99%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -6,6 +6,8 @@ class Solution {
         if (dp[i][j] != -1) return dp[i][j];
         
         int max = Integer.MIN_VALUE;
+
+        // k balloon burst krte time (i-j) ke bahar ki range k balloon are still alive cuz k balloon last me burst hoga
         for (int k = i; k <= j; k++){
             int cost = arr[i-1]*arr[k]*arr[j+1] + recursion(i, k-1, arr, dp) + recursion(k+1, j, arr, dp);
             max = Math.max(max, cost);
@@ -26,10 +28,28 @@ class Solution {
         }
 
         int[][] dp = new int[n+2][n+2];
-        for (int i = 0; i < n+2; i++){
-            Arrays.fill(dp[i], -1);
+        // for (int i = 0; i < n+2; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+
+        // return recursion(1, n, arr, dp);
+
+        int l = nums.length;
+
+        for (int i = l; i >= 1; i--){
+            for (int j = i; j <= l; j++){
+                int max = Integer.MIN_VALUE;
+
+                for (int k = i; k <= j; k++){
+                    int cost = arr[i-1]*arr[k]*arr[j+1] + dp[i][k-1] + dp[k+1][j];
+                    max = Math.max(cost, max);
+                }
+                dp[i][j] = max;
+            }
         }
 
-        return recursion(1, n, arr, dp);
+        return dp[1][l];
+
+
     }
 }
