7fcb529c97d2f9b276b363be51461576f9a01323
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-21 14:58:54 +0530

    Time: 27 ms (34.66%), Space: 44.8 MB (16.93%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    private int recursion(int i, int j , int[] cuts, int[][] dp){
+
+        if (i > j) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int min = Integer.MAX_VALUE;
+        for (int k = i; k <= j; k++){
+            // k denotes the first cut position:
+            int cost = cuts[j+1] - cuts[i-1] + recursion(i, k-1, cuts, dp) + recursion(k+1, j, cuts, dp);
+
+            // cuts[j+1] - cuts[i-1] is the length of the rod being cut
+            min = Math.min(min, cost); 
+        }
+
+        return dp[i][j] = min;
+    }
+    public int minCost(int n, int[] cuts) {
+        
+        Arrays.sort(cuts); // to make subproblems independent:
+        int[] cuts2 = new int[cuts.length+2];
+        cuts2[0] = 0;
+        cuts2[cuts2.length-1] = n;
+
+        for (int i = 1; i <= cuts.length; i++){
+            cuts2[i] = cuts[i-1];
+        }
+
+        int m = cuts2.length;
+
+        int[][] dp = new int[m][m];
+
+        for (int i = 0; i < m; i++){
+            Arrays.fill(dp[i], -1);
+        }
+
+        return recursion(1, cuts.length, cuts2, dp); // i, j, cuts
+    }
+}
