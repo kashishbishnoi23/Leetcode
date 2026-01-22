3fdbf7765fa12a53ff4c308a884c9eb8541c02c5
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-22 11:00:01 +0530

    Time: 4 ms (81.3%), Space: 47.7 MB (5.47%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,35 +1,54 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         int n = asteroids.length;
-
         Stack<Integer> st = new Stack<>();
 
-        for (int i = 0; i < n; i++){
-            int curr = asteroids[i];
-            if (st.isEmpty()){
+        int j = 0;
+        while(j < n && asteroids[j] < 0){
+           st.push(asteroids[j++]);
+        }
+
+        while(j < n){
+            int curr = asteroids[j];
+
+            if (curr > 0){
+                // agar positive hai -> simply push to the stack
                 st.push(curr);
-            } else {
-                // agar st me positive h aur bahar negative h -> collision:
-                while(!st.isEmpty() && st.peek() > 0 && curr < 0 && Math.abs(curr) > st.peek()){
-
-                    st.pop();
-                }
-
-                if (!st.isEmpty() && st.peek() > 0  && curr < 0 && Math.abs(curr) <= st.peek()){
-                    if (Math.abs(curr) == st.peek()){
-                        st.pop();
-                    }
-                    continue;
-                } 
-
-                st.push(curr);
+                j++;
+                continue;
             }
+
+            // agar negative hai aur st.peek() se chhota hai -> collide hoke khatam ho jayega:
+            if (!st.isEmpty() && Math.abs(curr) < st.peek()){
+                j++;
+                continue;
+            }
+
+            if (!st.isEmpty() && Math.abs(curr) == st.peek()){
+                st.pop();
+                j++;
+                continue;
+            }
+
+            // keep destroying the positives:
+            while(!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(curr)){
+                st.pop();
+            }
+
+            // ab ya to stack empty ho chuka hai -> ya usme negative elements hai ya equal strength ka positive element hai
+            if (st.isEmpty() || st.peek() < 0){
+                st.push(curr);
+            } else if (st.peek() == Math.abs(curr)){
+                st.pop();
+            }
+            j++;
+
         }
 
         int[] ans = new int[st.size()];
 
-        for (int j = ans.length-1; j >= 0; j--){
-            ans[j] = st.pop();
+        for (int i = ans.length-1; i >= 0; i--){
+            ans[i] = st.pop();
         }
 
         return ans;
