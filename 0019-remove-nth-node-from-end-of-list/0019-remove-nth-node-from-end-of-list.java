dd6b4ef7ec97e6ac7f856e128b829b3c5e1bbec0
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-25 23:47:44 +0530

    Time: 0 ms (100%), Space: 43.6 MB (47.28%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,27 +10,64 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int total = 0;
+        // int total = 0;
 
-        ListNode temp = head;
-        while(temp != null){
-            total++;
-            temp = temp.next;
-        }
+        // ListNode temp = head;
+        // while(temp != null){
+        //     total++;
+        //     temp = temp.next;
+        // }
 
-        int k = total-n;
-        if (k == 0) return head.next;
-        int count = 1;
-        temp = head;
+        // int k = total-n;
+        // if (k == 0) return head.next;
+        // int count = 1;
+        // temp = head;
 
-        while(count < k){
-            temp = temp.next;
-            count ++;
-        }
-        if (temp.next != null){
-            temp.next = temp.next.next;
-        }
+        // while(count < k){
+        //     temp = temp.next;
+        //     count ++;
+        // }
+        // if (temp.next != null){
+        //     temp.next = temp.next.next;
+        // }
 
-        return head;
+        // return head;
+
+        // one-pass:
+
+        // 2 pointers lo -> slow and fast 
+
+        // fast ko n steps chalao => ab fast slow se n steps aage hai -> ab slow aur fast ko ek sath chalao -> jase hi fast null hoga -> slow fast se n steps peeche hoga -> yani end se n steps peeche
+
+       ListNode slow = head;
+       ListNode fast = head;
+
+       int count = 0;
+
+       while(count < n){
+           fast = fast.next;
+           count++;
+       }
+
+       // if fast is null -> n is the length of linked list -> we want to delete the head
+       if (fast == null) return head.next;
+
+       while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+       }
+
+       slow.next = slow.next.next;
+
+       return head;
+
+
+
+
+
+
+
+
+
     }
 }
