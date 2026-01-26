1ac2e8e0becc4e7d473cab438be4fbb5e608834f
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-26 14:39:13 +0530

    Time: 0 ms (100%), Space: 46.9 MB (42.6%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) return true;
+            // cycle me aane k baad -> fast har step pe slow k ek step paas aata jayega -> cuz fast is taking one step more than slow
+
+
+        }
+
+        return false;
+    }
+}
