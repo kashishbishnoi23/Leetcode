4e9acddc84bf57fb353fac11c82398ae0cfb48d7
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-27 21:14:47 +0530

    Time: 1 ms (99.89%), Space: 53.1 MB (18.45%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -13,21 +13,61 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
         // store the nodes of one LL in a set, traverse through the second LL -> whichever common node comes first -> return 
-        HashSet<ListNode> st = new HashSet<>();
+        // HashSet<ListNode> st = new HashSet<>();
 
-        ListNode temp = headA;
-        while(temp != null){
-            st.add(temp);
-            temp = temp.next;
+        // ListNode temp = headA;
+        // while(temp != null){
+        //     st.add(temp);
+        //     temp = temp.next;
+        // }
+
+        // temp = headB;
+
+        // while(temp != null){
+        //     if (st.contains(temp)) return temp;
+        //     temp  = temp.next;
+        // }
+
+        // return null;
+
+        int count1 = 0;
+        ListNode temp1 = headA;
+
+        while(temp1 != null){
+            count1 ++;
+            temp1 = temp1.next;
         }
 
-        temp = headB;
+        int count2 = 0;
+        ListNode temp2 = headB;
 
-        while(temp != null){
-            if (st.contains(temp)) return temp;
-            temp  = temp.next;
+        while(temp2 != null){
+            count2 ++;
+            temp2 = temp2.next;
         }
 
-        return null;
+        int diff = Math.abs(count1 - count2);
+        temp1 = headA;
+        temp2 = headB;
+
+        if (count1 > count2){
+            
+            while(diff > 0){
+                temp1 = temp1.next;
+                diff --;
+            }
+        } else {
+            while(diff > 0){
+                temp2 = temp2.next;
+                diff --;
+            }
+        }
+
+        while(temp1 != temp2){
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return temp1;
     }
 }
