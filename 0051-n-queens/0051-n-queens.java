439ab6a4109294659211f787a36771363c416c04
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-28 01:56:40 +0530

    Time: 3 ms (46.11%), Space: 46.7 MB (56.34%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,64 +1,77 @@
 class Solution {
 
-    private void recursive(int n,int[][] filled, int row, List<List<String>> ans){
 
+    private void recursive(int[][] board, int row, List<List<String>> ans){
+        int n = board.length;
         if (row == n){
-            List<String> path = new ArrayList<>();
-            for (int i = 0; i < n; i++){
-                StringBuilder temp = new StringBuilder();
-                for (int j = 0; j < n; j++){
-                     if (filled[i][j] == 0){
-                        temp.append(".");
-                     } else {
-                        temp.append("Q");
-                     }
-                }
-                path.add(temp.toString());
+          List<String> temp = new ArrayList<>();
+          for (int i = 0; i < n; i++){
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++){
+                 if (board[i][j] == 0){
+                    sb.append('.');
+                 } else {
+                    sb.append('Q');
+                 }
             }
-            ans.add(path);
-            return;
+            temp.add(sb.toString());
+          }
 
+          ans.add(temp);
+          return;
         }
-         
-         // fill the current row and current col , then move to the next row:
-         // is it okay to fill the current col:
-         for (int col = 0; col < n; col++){
-            if (isValid(filled, row, col, n)){
-                filled[row][col] = 1;
-                // go to the next row:
-                recursive(n, filled, row+1, ans);
-                filled[row][col] = 0; // try another col and go again
 
+        for (int col = 0; col < n; col++){
+            // can I put the queen here:
+            if (isValid(board, row, col)){
+                board[row][col] = 1;
+                recursive(board, row+1, ans);
+                board[row][col] = 0;          
             }
-         }
+            
+        }
 
-
+        
     }
 
-    private boolean isValid(int[][] filled, int row, int col, int n){
+    private boolean isValid(int[][] board, int row, int col){
+        if (row == 0) return true;
+        int n = board.length;
+
         for (int i = 0; i < row; i++){
-            int diff = row-i;
-            int[] temp = {-diff, 0, diff};
-
-            for (int d : temp){
-                int newcol = col + d;
-                if (newcol >= 0 && newcol < n){
-                    if (filled[i][newcol] == 1) return false;
-                }
-
-            }
+            if (board[i][col] == 1) return false;
         }
+
+        int i = row;
+        int j = col;
+
+        while(i >= 0 && j >= 0){
+            if (board[i][j] == 1) return false;
+            i--;
+            j--;
+        }
+        i = row;
+        j = col;
+
+        while(i >= 0 && j < n){
+            if (board[i][j] == 1) return false;
+            i--;
+            j++;
+        }  
 
         return true;
     }
+
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        // List<String> temp = new ArrayList<>();
+          int[][] board = new int[n][n];
+          List<List<String>> ans = new ArrayList<>();
 
-        int[][] filled = new int[n][n];
 
-        recursive(n, filled, 0, ans);
+          recursive(board, 0, ans); // board and row
 
-        return ans;
+
+        
+
+          return ans;
     }
 }
