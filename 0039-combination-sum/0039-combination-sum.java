3729d4da9693eadb918646c263e57da66bd793fb
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-28 02:43:21 +0530

    Time: 2 ms (95.09%), Space: 45.9 MB (45.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,30 +1,37 @@
 class Solution {
-    private void recursion(int[] candidates, int indx, int sum, List<List<Integer>> ans, List<Integer> temp){
 
-        if (sum < 0 || indx == candidates.length){
+    private void recursive(int indx, int[] candidates, int target, List<List<Integer>> ans, List<Integer> temp){
+
+        if (target < 0 || indx == candidates.length) return;
+          
+          if (target == 0){
+            List<Integer> arr = new ArrayList<>(temp);
+            ans.add(arr);
             return;
-        }
-
-        if (sum == 0){
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
-
-        // pick
-        if (candidates[indx] <= sum){
-            temp.add(candidates[indx]);
-            recursion(candidates, indx, sum-candidates[indx], ans, temp);
+          }
+          // pick
+          temp.add(candidates[indx]);
+          recursive(indx, candidates, target-candidates[indx], ans, temp);
+          if (temp.size() >= 1){
             temp.remove(temp.size()-1);
-        }
+          }
 
-        // not pick
-        recursion(candidates, indx+1, sum, ans, temp);
-
+          // not pick
+          recursive(indx+1, candidates, target, ans, temp);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        recursion(candidates, 0, target, ans, temp);
+
+        recursive(0, candidates, target, ans, temp);
+
         return ans;
+
+
+        
+
+
+        // Time complexity -> O(2^(T/m + n)), where T is target, m is smallest number and n is totl number of elements -> let say m = 1, so depth becomes T and then -> not pick -> not pick -> and so on -> which adds n depth 
     }
 }
