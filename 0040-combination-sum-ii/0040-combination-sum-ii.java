c885224f2b01431ed4070890a1d49e66baa09715
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-28 03:03:20 +0530

    Time: 5 ms (99.11%), Space: 45 MB (83.96%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,29 +1,37 @@
 class Solution {
 
-    private void recursion(int[] candidates, int indx, int target, List<List<Integer>> ans, List<Integer> temp){
+    private void recursive(int[] candidates, int indx, List<List<Integer>> ans, List<Integer> temp, int target){
 
         if (target == 0){
-            ans.add(new ArrayList<>(temp));
+            List<Integer> curr = new ArrayList<>(temp);
+            ans.add(curr);
             return;
         }
-
-        // is current index pe -> ek baar jo element le liya -> vohi element is same index pe dobara nahi lenge:
-        for (int i = indx; i < candidates.length; i++){
-            //  if (i > 0 && candidates[])
-            if (i > indx && candidates[i] == candidates[i-1]) continue;
-            if (candidates[i] > target) break;
-            temp.add(candidates[i]);
-            recursion(candidates, i+1, target-candidates[i], ans, temp);
-            temp.remove(temp.size()-1);
-        }
-    }
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         
+        for (int i = indx; i < candidates.length; i++){
+            if (i > indx && candidates[i] == candidates[i-1]) continue;
+            if (candidates[i] > target) return;
+            temp.add(candidates[i]);
+            recursive(candidates, i+1, ans, temp, target-candidates[i]);
+            if (!temp.isEmpty()) temp.remove(temp.size()-1);
+        }
+
+    }
+
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
         Arrays.sort(candidates);
 
-        recursion(candidates, 0, target, ans, temp);
+        recursive(candidates, 0, ans, temp, target);
+
+
+        
+        
+
+        // time comp -> O(2^n) -> for loop isnt generating O(n) at each level -> its giving call for remaining children at each indx -> this is just a new way of generating all the subsets -> total nodes -> O(2^n) 
 
         return ans;
     }
