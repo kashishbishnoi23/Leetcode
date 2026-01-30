53898f9b36fbb426d4540af33aada9d893704f4f
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-01-30 11:15:35 +0530

    Time: 29 ms (90.34%), Space: 56.3 MB (78.74%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,131 +1,98 @@
 class Twitter {
 
-    Map<Integer, User> users;
-    int currtime = 0;
+    static int time = 0;
 
-    static class Post{
+    static class Post {
         int postId;
-        int userId;
-        int timeStamp;
-        public Post(int tweetId, int userId, int timeStamp){
-            this.postId = tweetId;
-            this.userId = userId;
-            this.timeStamp = timeStamp;
+        int time;
+
+        public Post(int postId, int time) {
+            this.postId = postId;
+            this.time = time;
         }
     }
 
-    PriorityQueue<Post> posts;
+    static class User {
+        int userId;
+        Set<User> followees;
+        List<Post> posts;
+
+        public User(int userId) {
+            this.userId = userId;
+            followees = new HashSet<>();
+            posts = new ArrayList<>();
+        }
+
+        public void addPost(int tweetId) {
+            posts.add(new Post(tweetId, time++));
+        }
+
+        public void follow(User user) {
+            if (user != this) {
+                followees.add(user);
+            }
+        }
+
+        public void unfollow(User user) {
+            followees.remove(user);
+        }
+    }
+
+    HashMap<Integer, User> users;
 
     public Twitter() {
         users = new HashMap<>();
-        posts = new PriorityQueue<>((a, b) -> {
-            return Integer.compare(b.timeStamp, a.timeStamp);
-        });
     }
 
-
-
-    static class User{
-        int userId;
-        List<Integer> Following = new ArrayList<>();
-        List<Post>posts = new ArrayList<>();
-
-        public User(int userID){
-            this.userId = userId;
-        }
-
-        public void addPost(Post post){
-            this.posts.add(post);
-        }
-
-        public void addFollowing(int userId){
-            this.Following.add(Integer.valueOf(userId));
-        }
-
-        public void removeFollowing(int userId){
-            this.Following.remove(Integer.valueOf(userId));
-        }
-
+    private User getUser(int userId) {
+        users.putIfAbsent(userId, new User(userId));
+        return users.get(userId);
     }
-    
+
     public void postTweet(int userId, int tweetId) {
-
-        currtime++;
-
-        // if user does'nt exist, add new user:
-        User curruser = users.containsKey(userId) ? users.get(userId) : null;
-
-        if (curruser == null){
-            curruser = new User(userId);
-            users.put(userId, curruser);
-        }
-
-        // add new post:
-        Post newpost = new Post(tweetId, userId, currtime);
-        curruser.addPost(newpost);
-        posts.offer(newpost);
-        
+        User user = getUser(userId);
+        user.addPost(tweetId);
     }
-    
+
     public List<Integer> getNewsFeed(int userId) {
+        List<Integer> result = new ArrayList<>();
+        if (!users.containsKey(userId)) return result;
 
-        if (!users.containsKey(userId)){
-            users.put(userId, new User(userId));
+        PriorityQueue<Post> pq = new PriorityQueue<>(
+            (a, b) -> b.time - a.time   
+        );
+
+        User user = users.get(userId);
+
+        // user's own posts
+        pq.addAll(user.posts);
+
+        // followees posts
+        for (User followee : user.followees) {
+            pq.addAll(followee.posts);
         }
 
-        List<Integer> Following = users.get(userId).Following;
-        List<Integer> news = new ArrayList<>();
-        User curr = users.get(userId);
-        if (Following.size() == 0 && curr.posts.size() == 0) return news;
-
-        List<Post> temp = new ArrayList<>();
-
-        int i = 0;
-        while(i < 10 && !posts.isEmpty()){
-            Post post = posts.peek();
-            int user = post.userId;
-            int postId = post.postId;
-            if (Following.contains(user) || user == userId){
-                System.out.println(postId);
-                news.add(postId);
-                i++;
-            }
-            temp.add(posts.poll());
-            System.out.println("size " + posts.size());
-            
+        int count = 0;
+        while (!pq.isEmpty() && count < 10) {
+            result.add(pq.poll().postId);
+            count++;
         }
 
-        for (int j = 0; j < temp.size(); j++){
-            posts.offer(temp.get(j));
-        }
-
-        temp.clear();
-
-        return news;
-
-
-
-
+        return result;
     }
-    
+
     public void follow(int followerId, int followeeId) {
-        if (!users.containsKey(followerId)){
-            users.put(followerId, new User(followerId));
-        }
-
-        if (!users.containsKey(followeeId)){
-            users.put(followeeId, new User(followeeId));
-        }
-
-       User user1 = users.get(followerId);
-       user1.addFollowing(followeeId);
+        User follower = getUser(followerId);
+        User followee = getUser(followeeId);
+        follower.follow(followee);
     }
-    
+
     public void unfollow(int followerId, int followeeId) {
-       User user1 = users.get(followerId);
-        user1.removeFollowing(followeeId);
+        if (!users.containsKey(followerId) || !users.containsKey(followeeId)) return;
+        users.get(followerId).unfollow(users.get(followeeId));
     }
 }
+
 
 /**
  * Your Twitter object will be instantiated and called as such:
