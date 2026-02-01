ee03e4d3cdcbb17c4fb760603086baf110e15888
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-02-02 00:13:02 +0530

    Time: 102 ms (31.67%), Space: 48.3 MB (8.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,39 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        if (s.length() <= 1) return s;
 
-        int maxLen = 1;
+
+    public String longestPalindrome(String s) {
+        
+        if (s.length() <= 1) return s;
+        int n = s.length();
+
+        int maxLength = 1;
+
         int start = 0;
         int end = 0;
-        boolean [][] dp = new boolean[s.length()][s.length()];
 
-        for (int i = 0; i < s.length(); i++){
-            dp[i][i] = true;
+        boolean[][] dp = new boolean[n][n];
 
+       // i -> end, j -> start
+        for (int i = 0; i < n; i++){
+            dp[i][i] = true; // single character is always palindrome
             for (int j = 0; j < i; j++){
-                if (s.charAt(i) == s.charAt(j) && (i-j <= 2 || dp[j+1][i-1])){
-                    dp[j][i] = true;
-                    if (i-j + 1 > maxLen){
-                        maxLen = i-j+1;
+                // kya j...i tak ka substring palindrome ha ya nahi ??
+                
+                // j is start, i is start => check if they match 
+                if (s.charAt(i) == s.charAt(j) && ((i-j) <= 2 || dp[j+1][i-1])){
+                    if ((i-j+1) > maxLength){
+                        maxLength = i-j+1;
                         start = j;
                         end = i;
                     }
+                    dp[j][i] = true;
                 }
             }
         }
 
         return s.substring(start, end+1);
+
+        
+    
     }
 }
