5442a620d386095fc9f7ced4589b1a45b5318247
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-02-01 11:33:45 +0530

    Time: 3 ms (23.2%), Space: 45.4 MB (72.8%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -2,10 +2,21 @@ class Solution {
     public int minimumCost(int[] nums) {
         int first = nums[0];
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
 
         for (int i = 1; i < nums.length; i++){
-            pq.offer(nums[i]);
+            // pq.offer(nums[i]);
+            int curr = nums[i];
+
+            if (pq.size() == 2){
+                if (curr < pq.peek()){
+                    pq.poll();
+                    pq.offer(curr);
+                }
+            }  else{
+                pq.offer(curr);
+            }
+
         }
 
         int cost = first;
