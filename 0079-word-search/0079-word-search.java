a373a2e33a66cdba3906a04dab47ebf59b21acd0
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-02-02 17:09:05 +0530

    Time: 231 ms (13.01%), Space: 46.8 MB (14.73%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,56 +1,56 @@
 class Solution {
+   
 
+   private boolean recursion(int row, int col, char[][] board, String word, int indx, int[][] visited){
 
-    private boolean recursive(int row, int col, char[][] board, String word, int indx, int[][] visited){
+      if (indx == word.length()) return true;
 
-        visited[row][col] = 1;
+      int n = board.length;
+      int m = board[0].length;
+       
+      visited[row][col] = 1;
 
-        if (indx == word.length()-1) return true;
+      int[][] dirs = {{0,1},{1, 0},{-1, 0}, {0, -1}};
 
-        int n = board.length;
-        int m = board[0].length;
+       // go to its neighbours:
+       for (int[] dir : dirs){
+           int newrow = row + dir[0];
+           int newcol = col + dir[1];
 
-        int[][] directions = {{0,1}, {1, 0}, {-1, 0}, {0, -1}};
-
-        for (int[] direction : directions){
-            int newrow = row + direction[0];
-            int newcol = col + direction[1];
-
-            if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && visited[newrow][newcol] == 0){
-                if (board[newrow][newcol] == word.charAt(indx+1)){
-                    if (recursive(newrow, newcol, board, word, indx+1, visited) == true){
-                        return true;
-                    }
-                }
+           if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && visited[newrow][newcol] == 0){
+            if (board[newrow][newcol] == word.charAt(indx)){
+                if (recursion(newrow, newcol, board, word, indx+1, visited) == true) return true;
             }
-        }
+           }
+       }
 
-        visited[row][col] = 0;
 
-        return false;
+       visited[row][col] = 0;
 
-    }
+
+       return false;
+
+   }
 
  
 
     public boolean exist(char[][] board, String word) {
         
+       int n = board.length;
+       int m = board[0].length;
 
-        int n = board.length;
-        int m = board[0].length;
-
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (board[i][j] == word.charAt(0)){
-                    int[][] visited = new int[n][m];
-                    if (recursive(i, j, board, word, 0, visited) == true){
-                        return true;
-                    }
-                }
+       for (int i = 0; i < n; i++){
+         for (int j = 0; j < m; j++){
+            if (board[i][j] == word.charAt(0)){
+                int[][] visited = new int[n][m];
+                if (recursion(i, j, board, word, 1, visited) == true) return true;
+                
             }
-        }
 
-        return false;
+         }
+       }
 
+       return false;
+    
     }
 }
