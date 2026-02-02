e02c66d4b1acadaa7cbf36674a27cd2371532990
Kashish Bishnoi <156753341+kashishbishnoi23@users.noreply.github.com>
2026-02-03 01:17:36 +0530

    Time: 593 ms (82.35%), Space: 50.8 MB (78.59%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -1,131 +1,109 @@
 class Solution {
     public int minimumDifference(int[] nums) {
-        int n = nums.length / 2;
-
-        // basically, hame n size ke 2 subsets chahiye -> jinke sum ka difference minimum ho -> maanlo n size k ek subset ka sum x hai -> to dusre bache hue elements ka sum S-x ho jayega where S is the total sum of all elements -> we need to minimize -> (S-x) - x = S - 2x , so ham sirf n elements ke liye har possible x nikalenge -> aur har possible S-2x ki value nikalenge and the minimum will be our answer
-
-        // iske liye ham nums ko 2 parts me baantenge => left and right => each will have n elements -> ab left subset me har size k liye we will find all the possible sums 
-
-        // for example, we have nums => {3,9,7,3}
-        // left = {3, 9}  right = {7, 3}
-
-        // ab left subset ke liye -> for size 0, only possible sum is 0
-        // for size 1 -> {3, 9}
-        // for size 2 -> {12}
-
-        // so for left subset we have -> {{0}, {3, 9}, {12}}
-
-        // now -> if I pick 0 elements from left, I need to pick n from right subset , in that way I will make a subset of {7, 3}
-
-        // if I pick 1 element from left -> eg, 3, I can pick 1 from right, so we will get 2 sums (x ) -> 3 + 7 = 10 and 3 + 3 = 6
-        // and so on..., this is how we create all the x values 
+        int n = nums.length/2;
 
         int total = 0;
-        for (int num : nums){
-            total += num;
+        for (int a : nums){
+            total += a;
         }
 
-        int[] leftArr = Arrays.copyOfRange(nums, 0, n);
-        int[] rightArr = Arrays.copyOfRange(nums, n, 2*n );
-
-        ArrayList<ArrayList<Integer>> left = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> right = new ArrayList<>();
+        // divide into 2 arrays -> left and right:
+        int[] left = Arrays.copyOfRange(nums,0, n);
+        int[] right = Arrays.copyOfRange(nums,n, 2*n);
+        
+        // left me -> kis length pe kitna sum mil skta hai -> vo calculate kro:
+        ArrayList<ArrayList<Integer>> leftArraySums = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> rightArraySums = new ArrayList<>();
 
         for (int i = 0; i <= n; i++){
-            left.add(new ArrayList<>());
-            right.add(new ArrayList<>());
+            leftArraySums.add(new ArrayList<>());
+            rightArraySums.add(new ArrayList<>());
         }
 
-        // Generate subsets for left: total 2^n combinations possible
-        for (int mask = 0; mask <= (1 << n); mask++){
+
+        // now length is n -> so we can generate 2^n combinations / subsets -> vo sare combinations ke sums -> length k according store krao in left and right array sums
+
+        for (int mask = 0; mask < (1 << n); mask++){
             int size = 0;
             int sum = 0;
             for (int i = 0; i < n; i++){
-                if (((1 << i) & mask) != 0){
+                // can I take the ith index??
+                if ((mask & (1 << i)) != 0){
                     size ++;
-                    sum += leftArr[i];
+                    sum += left[i];
                 }
             }
-            left.get(size).add(sum);
+
+            leftArraySums.get(size).add(sum);
         }
-        
-        // Generate subsets for right:
-        for (int mask = 0; mask <= (1 << n); mask++){
+
+        for (int mask = 0; mask < (1 << n); mask++){
             int size = 0;
             int sum = 0;
-
-             for (int i = 0; i < n; i++){
-                if (((1 << i) & mask) != 0){
+            for (int i = 0; i < n; i++){
+                // can I take the ith index??
+                if ((mask & (1 << i)) != 0){
                     size ++;
-                    sum += rightArr[i];
+                    sum += right[i];
                 }
             }
-            right.get(size).add(sum);
 
+            rightArraySums.get(size).add(sum);
         }
 
         for (int i = 0; i <= n; i++){
-            Collections.sort(right.get(i));
+            Collections.sort(rightArraySums.get(i));
         }
-
+        
         int ans = Integer.MAX_VALUE;
 
         for (int i = 0; i <= n; i++){
-            for (int leftSum : left.get(i)){
-                // i length pe ye sum hai -> leftSum
 
-                // now I need a sum of length n-i
+            // i is the size/ number of elements I will pick from the left array
+            for (int sum : leftArraySums.get(i)){
+                // ith size pe ye ye sums available hain:
 
-                int reqLength = n-i;
-                ArrayList<Integer> rightSums = right.get(reqLength);
+                int leftSum = sum;
+                int req = n-i;
 
-                // int x = leftSum;
-
-                // for (int rightSum : rightSums){
-                //     x = leftSum + rightSum;
-                //     ans = Math.min(ans, Math.abs(total - 2*x));
-                // }
-
-                // find the best rightSum -> closer to target , where target is total/2 - x(leftsum)
-
+                // pick the best sum from rightArraySums such that leftSum + rightSum is closer to total/2
                 int target = total/2 - leftSum;
-                // Collections.sort(rightSums);
-                int x = 0;
+                ArrayList<Integer> rightSums = rightArraySums.get(req);
+
+                int index = binarySearch(rightSums, target);
                 int closestR = 0;
 
-                int indx = binarySearch(rightSums, target);
-
-                if (indx >= 0 && indx < rightSums.size() && rightSums.get(indx) == target){
-                    // ans = Math.min(ans, );
-                    return total % 2 == 0 ? 0 : 1;
-
+                if (index >= 0 && index < rightSums.size() && rightSums.get(index) == target){
+                    return (total % 2 == 0) ? 0 : 1;
                 } else {
-                    // we have got insertion point 
-                    if (indx == rightSums.size()){ // yani target is greater than all the sums -> so x should be rightSums.get(indx-1)
-                        closestR = rightSums.get(indx-1);
-                    } else if (indx == -1){
-                        closestR = rightSums.get(indx+1);
-                    } else if (indx == 0){
-                        closestR = rightSums.get(indx);
-                    }
-                    else {
-                        // we have two points -> indx -1 and indx + 1
+                    // index is the insertion point of that target:
+                    if (index == rightSums.size()){
+                        closestR = rightSums.get(index-1);
+                    } else if (index == 0) {
+                        closestR = rightSums.get(index);
+                    } else if (index == -1){
+                        closestR = rightSums.get(index+1);
+                    } else {
 
-                        int smaller = rightSums.get(indx-1);
-                        int larger = rightSums.get(indx);
+                        int smallest = rightSums.get(index-1);
+                        int largest = rightSums.get(index);
 
-                        if (Math.abs(target - smaller) < Math.abs(target - larger)){
-                            closestR = smaller;
+                        if (Math.abs(target - smallest) < Math.abs(target - largest)){
+                            closestR = smallest;
                         } else {
-                            closestR = larger;
+                            closestR = largest;
                         }
                     }
                 }
 
-                // ans = Math.min(ans,  total/2 )
-                x = leftSum + closestR;
+                int x = leftSum + closestR;
 
                 ans = Math.min(ans, Math.abs(total-2*x));
+
+
+
+
+
 
 
             }
@@ -135,31 +113,28 @@ class Solution {
 
     }
 
-    private int binarySearch(ArrayList<Integer> rightSum, int target){
-        int l = 0;
-        int r = rightSum.size()-1;
+    private int binarySearch(ArrayList<Integer> rightSums, int target){
 
-        while(l <= r){
-            int mid = l + (r-l)/2;
+        int start = 0;
+        int end = rightSums.size()-1;
 
-            if (rightSum.get(mid) == target){
-                return mid; // if found -> we return the index where it is found
-            } else if (rightSum.get(mid) < target){
-                l = mid + 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if (rightSums.get(mid) == target) return mid;
+
+            if (rightSums.get(mid) > target){
+                end = mid-1;
             } else {
-                r = mid - 1;
+                start = mid+1;
             }
         }
 
-        // if exact number is not found -> we have our l and r 
-
-        // where does l point to ?? -> the insertion point  iske peeche ka element is smaller than target -> iske aage ka 
-
-
-        return l;
-
+        return start;
 
 
 
     }
+
+
 }
